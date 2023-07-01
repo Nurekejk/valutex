e6f2c9c5d2ec423be59c459a6e0ca192743e17a1
abitoffka <aigerim.abitayevaa@gmail.com>
2023-07-02 01:46:56 +0600

    [KK-6] Создала папку Signup и добавила SignupViewController

---
+++ kurs.kz/SignUp/SignupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignupViewController.swift
+//  kurs.kz
+//
+//  Created by Aigerim Abitayeva on 02.07.2023.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
