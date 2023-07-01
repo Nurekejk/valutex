45408462e2f822b198001d77629b01be1a767db5
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-02 00:10:22 +0600

    создал класс RegistrationPersonalDataViewController с UITextField

--- kurs.kz/Registration/RegistrationPersonalDataViewController.swift
+++ kurs.kz/Registration/RegistrationPersonalDataViewController.swift
@@ -7,3 +7,36 @@
 
 import UIKit
 import SnapKit
+
+class RegistrationPersonalDataViewController: UIViewController {
+    
+    let surnameTextField: UITextField = {
+        let textField = UITextField(frame: CGRect(x: 50, y: 100, width: 200, height: 30))
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Enter text"
+        return textField
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        setupLayout()
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+    
+    func setup() {
+        view.addSubview(surnameTextField)
+    }
+    
+    func setupLayout() {
+        surnameTextField.snp.makeConstraints { make in
+            make.width.height.equalTo(50)
+            make.center.equalTo(self.view)
+        }
+        
+    }
+    
+
+
+}
