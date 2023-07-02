bd63ee374074b912135ed48b165cd5237457673b
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-03 02:43:35 +0600

    [KK-12] удалил весь код

--- kurs.kz/LanguageSelection/LanguageSelectViewController.swift
+++ kurs.kz/LanguageSelection/LanguageSelectViewController.swift
@@ -8,44 +8,4 @@
 import UIKit
 import SnapKit
 
-class LanguageSelectViewController: UIViewController {
-    
-    let label: UILabel = {
-        let label = UILabel()
-        label.text = "Выберите язык приложения"
-        label.font = .boldSystemFont(ofSize: 24)
-//        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        view.backgroundColor = .white
-        
-        setupViews()
-        setupConstraints()
-        
-    }
-    
-    private func setupViews() {
-        view.addSubview(label)
-    }
-    
-    private func setupConstraints() {
-//        NSLayoutConstraint.activate([
-//            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-//            label.trailingAnchor.constraint(equalTo: view.leadingAnchor, constant: -20),
-//            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-//        ])
-        
-        label.snp.makeConstraints { make in
-//            make.leading.equalToSuperview().offset(24)
-            make.top.equalToSuperview().offset(292)
-            make.centerX.equalToSuperview()
-            
-            
-        }
-    }
-}
