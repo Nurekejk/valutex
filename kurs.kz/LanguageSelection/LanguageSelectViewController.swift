4b9601a9fd051471f074f1336dbe3867e1639bf7
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-02 13:07:22 +0600

    [KK-12] создал котроллер добавил label

---
+++ kurs.kz/LanguageSelection/LanguageSelectViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LanguageSelectViewController.swift
+//  kurs.kz
+//
+//  Created by Bekzhan Askerbekov on 2/7/23.
+//
+
+import UIKit
+
+class LanguageSelectViewController: UIViewController {
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Выберите язык приложения"
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+
+        
+    }
+
+}
