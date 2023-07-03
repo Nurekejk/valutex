21fb0680b5a4c7409d039ec9c1db1532a0b43072
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-03 15:18:41 +0600

    создал RegistrationPasswordViewController

---
+++ kurs.kz/Registration/RegistrationPasswordViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegistrationPasswordViewController.swift
+//  kurs.kz
+//
+//  Created by MacBook on 03.07.2023.
+//
+
+import Foundation
+
+import UIKit
+import SnapKit
+
+class RegistrationPasswordViewController: UIViewController {
+    
+}
