58da9c1bf5bad580ba3a46b65ec54c22e4a99043
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-04 16:21:17 +0600

    [KK-12] поменял class на final class

--- kurs.kz/SelectLanguage/SelectLanguageViewController.swift
+++ kurs.kz/SelectLanguage/SelectLanguageViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import SnapKit
 
-class SelectLanguageViewController: UIViewController {
+final class SelectLanguageViewController: UIViewController {
     
     let imageView: UIImageView = {
         let imageView = UIImageView()
