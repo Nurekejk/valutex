a9631cbb2560217b89e0b928692dfa002cfbf708
abitoffka <aigerim.abitayevaa@gmail.com>
2023-07-04 19:52:14 +0600

    [KK-6] Поменяла class->final class, open-> public в CustomSkyFloatingLabelTextField

--- kurs.kz/SignUp/CustomSkyFloatingLabelTextField.swift
+++ kurs.kz/SignUp/CustomSkyFloatingLabelTextField.swift
@@ -8,23 +8,23 @@
 import UIKit
 import SkyFloatingLabelTextField
 
-class CustomSkyFloatingLabelTextField: SkyFloatingLabelTextField {
+final class CustomSkyFloatingLabelTextField: SkyFloatingLabelTextField {
     
         let padding = UIEdgeInsets(top: 10, left: 16, bottom: 0, right: 0)
 
-        override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        override public func textRect(forBounds bounds: CGRect) -> CGRect {
             return bounds.inset(by: padding)
         }
 
-        override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        override public func placeholderRect(forBounds bounds: CGRect) -> CGRect {
             return bounds.inset(by: padding)
         }
 
-        override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        override public func editingRect(forBounds bounds: CGRect) -> CGRect {
             return bounds.inset(by: padding)
         }
     
-        override open func titleLabelRectForBounds(_ bounds: CGRect, editing: Bool) -> CGRect {
+        override public func titleLabelRectForBounds(_ bounds: CGRect, editing: Bool) -> CGRect {
             if editing {
                 return CGRect(x: 16, y: 8, width: bounds.size.width, height: titleHeight())
             }
