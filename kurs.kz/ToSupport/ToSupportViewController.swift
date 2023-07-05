9fc687e38cc2ba310261a8668f2078eaed9cb079
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-05 13:26:29 +0600

    [KK-26] создаю кнопку и добавляю assets изображени

--- kurs.kz/ToSupport/ToSupportViewController.swift
+++ kurs.kz/ToSupport/ToSupportViewController.swift
@@ -6,10 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 final class ToSupportViewController: UIViewController {
     
-    
+    private let telegramButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Telegram", for: .normal)
+        button.setImage(UIImage(named: "telegram_icon"), for: .normal)
+        button.tintColor = .black
+        button.imageView?.contentMode = .scaleAspectFit
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 10
+        button.layer.borderWidth = 2
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
