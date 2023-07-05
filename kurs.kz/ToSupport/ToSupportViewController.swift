00b7e3f9fdc769c58448219a0bd7233fb926531f
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-05 12:53:57 +0600

    [KK-26] создал новый ToSupportViewController

---
+++ kurs.kz/ToSupport/ToSupportViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ToSupportViewController.swift
+//  kurs.kz
+//
+//  Created by Bekzhan Askerbekov on 5/7/23.
+//
+
+import UIKit
+
+class ToSupportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
