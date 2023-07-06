7958db685dcc6c808c67d6e6d355bc388d05e066
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-06 16:22:49 +0600

    создал класс StarButton

---
+++ kurs.kz/Exchange Card/StarButton.swift
@@ -0,0 +1,25 @@
+//
+//  StarButton.swift
+//  kurs.kz
+//
+//  Created by MacBook on 06.07.2023.
+//
+
+import UIKit
+
+final class StarButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButton()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupButton()
+    }
+
+    func setupButton() {
+        setImage(UIImage(named: "gray_star"), for: .normal)
+    }
+}
