84fd9c91c2124baf09243f24039ca946ce166523
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-06 16:24:06 +0600

    создал array starArray и starStack для кнопок

--- kurs.kz/Exchange Card/RateViewController.swift
+++ kurs.kz/Exchange Card/RateViewController.swift
@@ -1,5 +1,17 @@
 import UIKit
 
 final class RateViewController: UIViewController {
+    // MARK: - UI
+    private var starArray = [StarButton]()
+    
+    private let starStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 4
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.backgroundColor = .red
+        return stackView
+    }()
 
 }
