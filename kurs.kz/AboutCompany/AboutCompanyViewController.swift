a7e0cf0b3a57431808a49bc266228f84d23eddd4
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-07 12:01:53 +0600

    [KK-27] Создал новый AboutCompanyViewController

---
+++ kurs.kz/AboutCompany/AboutCompanyViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AboutCompanyViewController.swift
+//  kurs.kz
+//
+//  Created by Bekzhan Askerbekov on 7/7/23.
+//
+
+import UIKit
+
+class AboutCompanyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
