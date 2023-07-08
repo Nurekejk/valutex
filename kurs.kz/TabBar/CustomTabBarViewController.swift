0cd4110f5ba7b306ad8fa42793874f8c28cefeff
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-08 12:25:30 +0600

    [KK-30] добавил базовый функционал таб бара

--- kurs.kz/TabBar/CustomTabBarViewController.swift
+++ kurs.kz/TabBar/CustomTabBarViewController.swift
@@ -12,20 +12,39 @@ class CustomTabBarViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupTabs()
         // Do any additional setup after loading the view.
     }
     
     func setupTabs() {
-//        [RegistrationPasswordViewController(), RegistrationPersonalDataViewController(), SelectLanguageViewController()].forEach { self.setViewControllers($0, animated: true)}
+        let home = createNavigation(mainTitle: "Home", title: "Hoooooome",
+                                    image: UIImage(named: "home_selected")!,
+                                    viewController: RegistrationPasswordViewController())
+        let money = createNavigation(mainTitle: "Money", title: "OOOOO",
+                                    image: UIImage(named: "money_gray")!,
+                                     viewController: RegistrationPersonalDataViewController())
+        let other = createNavigation(mainTitle: "Other", title: "MOOON",
+                                    image: UIImage(named: "other_gray")!,
+                                     viewController: SelectLanguageViewController())
+        self.setViewControllers([home,money,other], animated: true)
+//        [home, money, other].forEach
+//        { self.setViewControllers($0, animated: true)}
     }
     
     func createNavigation(mainTitle: String, title: String, image: UIImage,
                           viewController: UIViewController) -> UINavigationController {
         let navigationController = UINavigationController(rootViewController: viewController)
+        
         navigationController.tabBarItem.title = title
         navigationController.tabBarItem.image = image
         navigationController.navigationItem.title = mainTitle
+        
+// navigationController.viewControllers.first?.title = mainTitle + " Controller"
+        navigationController.viewControllers.first?.navigationItem.rightBarButtonItem =
+        UIBarButtonItem(title: "Button", style: .plain, target: nil, action: nil)
+        navigationController.viewControllers.first?.navigationItem.rightBarButtonItem?.tintColor =
+            .green
+        return navigationController
     }
 
     /*
