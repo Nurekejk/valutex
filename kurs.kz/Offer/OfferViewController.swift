f1e5c3ec58151736b4390d50506733af02754b37
Nurbol Zhetpisbay <zhetpisbaynurbol@gmail.com>
2023-07-09 16:54:08 +0600

    [KK-31] Создал OfferViewController

---
+++ kurs.kz/Offer/OfferViewController.swift
@@ -0,0 +1,19 @@
+//
+//  OfferViewController.swift
+//  kurs.kz
+//
+//  Created by Nurbol on 09.07.2023.
+//
+
+import UIKit
+
+class OfferViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Navigation
+}
