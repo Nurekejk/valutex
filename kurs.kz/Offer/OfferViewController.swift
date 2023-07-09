6eadf35b764c3205d1f6a53107736761205c6f2b
Nurbol Zhetpisbay <zhetpisbaynurbol@gmail.com>
2023-07-09 17:09:42 +0600

    [KK-31] Создал Label (Отклики)

--- kurs.kz/Offer/OfferViewController.swift
+++ kurs.kz/Offer/OfferViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class OfferViewController: UIViewController {
+final class OfferViewController: UIViewController {
 
-    
+    // MARK: - Outlets
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Отклики (2)"
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.numberOfLines = 0
+        return label
+    }()
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+        setupConctraints()
+    }
+    
+    // MARK: - Setup
+    private func setupViews() {
+        view.backgroundColor = .systemGray6
+        view.addSubview(label)
+    }
+
+    private func setupConctraints() {
+        label.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(332)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-48)
+            make.height.equalTo(20)
+        }
 
     }
 
-    // MARK: - Navigation
 }
