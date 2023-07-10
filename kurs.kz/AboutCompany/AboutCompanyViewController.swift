f4164aa8ec3d6345021f5d16f3bf37f9756c146e
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-10 20:30:12 +0600

    [KK-27] исправил коментарии

--- kurs.kz/AboutCompany/AboutCompanyViewController.swift
+++ kurs.kz/AboutCompany/AboutCompanyViewController.swift
@@ -19,7 +19,7 @@ final class AboutCompanyViewController: UIViewController {
         label.textAlignment = .center
         return label
     }()
-
+    
     private let aboutCompanyTextView: UITextView = {
         let textView = UITextView()
         textView.text = """
@@ -38,31 +38,38 @@ final class AboutCompanyViewController: UIViewController {
     }()
     
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
-        self.title = "О компании"
-        setupLayout()
+        setupNavigationBar()
+        setupView()
+        setupConstraints()
     }
-
+    
     // MARK: - Setup
     
-    private func setupLayout() {
+    private func setupNavigationBar() {
+        self.view.backgroundColor = .white
+        self.title = "О компании"
+    }
+        
+    private func setupView() {
         view.addSubview(aboutCompanyLabel)
         view.addSubview(aboutCompanyTextView)
+    }
         
+    private func setupConstraints() {
         aboutCompanyLabel.snp.makeConstraints {make in
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
-        }
-
+            }
+            
         aboutCompanyTextView.snp.makeConstraints { make in
             make.top.equalTo(aboutCompanyLabel.snp.bottom).offset(16)
             make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
             make.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
             make.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-16)
+            }
         }
     }
-}
