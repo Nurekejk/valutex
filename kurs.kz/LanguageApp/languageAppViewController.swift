27abe1dceecc33c056ca75799a4a91535abdcfcf
diarorynbek <dikodiko901123@gmail.com>
2023-07-10 11:28:16 +0600

    [KK-33] создал папку

---
+++ kurs.kz/LanguageApp/languageAppViewController.swift
@@ -0,0 +1,29 @@
+//
+//  languageAppViewController.swift
+//  kurs.kz
+//
+//  Created by Diar Orynbek on 10.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class languageAppViewController: UIViewController {
+  
+  //MARK: -UI
+  
+  //MARK: -LifeCycle
+  
+  //MARK: -Setup Views
+  
+  private func setupViews() {
+    
+  }
+  
+  //MARK: -Setup Constraints
+  
+  private func setupConstraints() {
+    
+  }
+  
+}
