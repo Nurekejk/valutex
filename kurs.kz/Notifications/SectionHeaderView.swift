c781e1912657ec9dd1514809fddee385539fcd8e
Akerke <nassyr.akyerke@gmail.com>
2023-07-11 23:06:37 +0600

    [KK-29] Создала файл SectionHeaderView

---
+++ kurs.kz/Notifications/SectionHeaderView.swift
@@ -0,0 +1,42 @@
+//
+//  SectionHeaderView.swift
+//  kurs.kz
+//
+//  Created by Акерке on 11.07.2023.
+//
+
+import UIKit
+
+class SectionHeaderView: UIView {
+
+    private lazy var titleLabel: UILabel = {
+            let label = UILabel()
+            label.text = "Сегодня"
+            label.font = .systemFont(ofSize: 14)
+            return label
+        }()
+
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+
+            setupViews()
+            setupConstraints()
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+    // MARK: - Setup Views
+        private func setupViews() {
+            
+            addSubview(titleLabel)
+        }
+
+        // MARK: - Setup Constraints
+        private func setupConstraints() {
+            titleLabel.snp.makeConstraints { make in
+                make.center.equalToSuperview()
+            }
+        }
+    }
