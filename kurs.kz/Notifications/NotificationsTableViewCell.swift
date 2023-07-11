9cde7d62456aba13e63f2bbedb8746db52800eff
Akerke <nassyr.akyerke@gmail.com>
2023-07-11 16:12:03 +0600

    [KK-29] Cоздала NotificationTableViewCell

--- kurs.kz/Notifications/NotificationsTableViewCell.swift
+++ kurs.kz/Notifications/NotificationsTableViewCell.swift
@@ -6,18 +6,56 @@
 //
 
 import UIKit
+import SnapKit
 
-class NotificationsTableViewCell: UITableViewCell {
+final class NotificationsTableViewCell: UITableViewCell {
+    // MARK: - UI
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Тех. поддержка"
+        label.font = .boldSystemFont(ofSize: 16)
+        return label
+    }()
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private lazy var textField: UITextField = {
+            let textField = UITextField()
+            textField.backgroundColor = .systemGray6
+            textField.textAlignment = .left
+            textField.placeholder = "Добро пожаловать в службу поддержки"
+            return textField
+        }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupViews()
+        setupConstraints()
+
+    }
+    required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+    // MARK: - Setup Views
+
+    private func setupViews() {
+        contentView.addSubview(titleLabel)
+
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    // MARK: - Setup Constraints
 
-        // Configure the view for the selected state
+    private func setupConstraints() {
+
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-40)
+
     }
+        textField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-40)
+        }
 
-}
