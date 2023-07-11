0f18db12a76d3a6b366595b8bb108c0f21573325
Akerke <nassyr.akyerke@gmail.com>
2023-07-11 16:12:49 +0600

    [KK-29] Cоздала NotificationsViewController

--- kurs.kz/Notifications/NotificationsViewController.swift
+++ kurs.kz/Notifications/NotificationsViewController.swift
@@ -7,23 +7,50 @@
 
 import UIKit
 
-class NotificationsViewController: UIViewController {
+final class NotificationsViewController: UIViewController {
+    // MARK: - UI
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.dataSource  = self
+        tableView.delegate = self
+        return tableView
+    }
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
+
     }
-    
+    // MARK: - Setup Views
 
-    /*
-    // MARK: - Navigation
+    private func setupViews() {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+    // MARK: - Setup Constraints
+
+    private func setupConstraints() {
+
+    }
 }
+
+    // MARK: - UITableViewDataSource, UITableViewDelegate
+
+extension NotificationsViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+       return 3
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return 1
+
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+}
+
+
