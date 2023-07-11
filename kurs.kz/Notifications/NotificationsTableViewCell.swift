a78703294eefa11f490182566dbeae4613557ef4
Akerke <nassyr.akyerke@gmail.com>
2023-07-11 16:50:12 +0600

    [KK-29] Добавила subTitleLabel

--- kurs.kz/Notifications/NotificationsTableViewCell.swift
+++ kurs.kz/Notifications/NotificationsTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 import SnapKit
 
 final class NotificationsTableViewCell: UITableViewCell {
+
     // MARK: - UI
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
@@ -16,46 +17,50 @@ final class NotificationsTableViewCell: UITableViewCell {
         label.font = .boldSystemFont(ofSize: 16)
         return label
     }()
-
-    private lazy var textField: UITextField = {
-            let textField = UITextField()
-            textField.backgroundColor = .systemGray6
-            textField.textAlignment = .left
-            textField.placeholder = "Добро пожаловать в службу поддержки"
-            return textField
-        }()
+    private lazy var subTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Добро пожаловать в службу поддержки"
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-
+        
         setupViews()
         setupConstraints()
-
+        
     }
     required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func prepareForReuse() {
+            super.prepareForReuse()
+
         }
 
     // MARK: - Setup Views
-
+    
     private func setupViews() {
         contentView.addSubview(titleLabel)
-
+        contentView.addSubview(subTitleLabel)
+        
     }
-
+    
     // MARK: - Setup Constraints
-
+    
     private func setupConstraints() {
-
+        
         titleLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(16)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-40)
-
-    }
-        textField.snp.makeConstraints { make in
+            
+        }
+        subTitleLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(8)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-40)
         }
-
+    }
+}
