5669d246b335d75459e3ffc3c4ef68c4269aefab
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-13 01:31:12 +0600

    [KK-36] убрал навигейшн враппер

--- kurs.kz/SceneDelegate.swift
+++ kurs.kz/SceneDelegate.swift
@@ -18,8 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
 
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = UINavigationController(
-            rootViewController: CurrencySelectorViewController())
+        window?.rootViewController = CurrencySelectorViewController()
         window?.makeKeyAndVisible()
     }
 }
