9e25e70c3c77faa6c849f11c3cd8017417e2627d
abitoffka <aigerim.abitayevaa@gmail.com>
2023-07-13 01:24:17 +0600

    [KK-35] Добавила ProfileTableHeaderView

---
+++ kurs.kz/Profile/ProfileTableHeaderView.swift
@@ -0,0 +1,86 @@
+//
+//  ProfileTableHeaderView.swift
+//  kurs.kz
+//
+//  Created by Aigerim Abitayeva on 12.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class ProfileTableHeaderView: UITableViewHeaderFooterView {
+    
+    // MARK: - State
+    static let reuseID = String(describing: ProfileTableHeaderView.self)
+    
+    // MARK: - UI
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "avatar"))
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private lazy var fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Aidos Tazhigulov"
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = UIColor(named: "fullNameColor")
+        return label
+    }()
+    
+    private lazy var phoneNumberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "+7(707) 303-22-00"
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = UIColor(named: "phoneNumberColor")
+        return label
+    }()
+    
+    private lazy var userInformationStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.alignment = .center
+        stackView.spacing = 0
+        return stackView
+    }()
+    
+    // MARK: - Initializers
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setupConstraints()
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Views
+    private func setupViews() {
+        userInformationStackView.addArrangedSubview(avatarImageView)
+        userInformationStackView.addArrangedSubview(fullNameLabel)
+        userInformationStackView.addArrangedSubview(phoneNumberLabel)
+        
+        contentView.addSubview(userInformationStackView)
+    }
+    
+    // MARK: - Setup Constraints
+    private func setupConstraints() {
+        userInformationStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.leading.equalToSuperview().offset(94)
+            make.trailing.equalToSuperview().offset(-94)
+            make.height.equalTo(148)
+        }
+        
+        avatarImageView.snp.makeConstraints { make in
+            make.height.equalTo(80)
+            make.width.equalTo(80)
+        }
+    }
+}
