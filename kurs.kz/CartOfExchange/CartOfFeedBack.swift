e6e2435c043cc8ba50d435c7004d8e63c75cef3e
diarorynbek <dikodiko901123@gmail.com>
2023-07-13 11:54:40 +0600

    [KK-38] создал папку

---
+++ kurs.kz/CartOfExchange/CartOfFeedBack.swift
@@ -0,0 +1,33 @@
+//
+//  CartOfFeedBack.swift
+//  kurs.kz
+//
+//  Created by Diar Orynbek on 13.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class CartOfFeedBack: UIViewController {
+    
+    //MARK: - UI
+    
+    
+    //MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Setup Views
+    
+    func setupViews() {
+        
+    }
+    
+    //MARK: - Setup Constraits
+    
+    func setupConstraits() {
+        
+    }
+}
