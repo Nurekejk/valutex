01834953d62b260832dc2569270c6221a242854c
Akerke <nassyr.akyerke@gmail.com>
2023-07-13 16:01:44 +0600

    [KK-37] Добавила UIimage, texField для USD

--- kurs.kz/ExchangeRate/ExchangeRateTableViewCell.swift
+++ kurs.kz/ExchangeRate/ExchangeRateTableViewCell.swift
@@ -19,19 +19,38 @@ class ExchangeRateTableViewCell: UITableViewCell {
         let view = UIView()
         return view
     }()
-    private lazy var titleLabel: UILabel = {
+
+    private let USDFlagImage: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFit
+        image.image = UIImage(named: "USDFlag")
+        return image
+    }()
+
+    private lazy var currencyUSDLabel: UILabel = {
         let label = UILabel()
         label.text = "Доллар"
         label.font = .systemFont(ofSize: 16)
         return label
     }()
 
-    private lazy var titleFLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Доллар"
-        label.font = .systemFont(ofSize: 16)
-        return label
+    private lazy var amountOfPurchaseUSDTextField: UITextField = {
+        let textField = UITextField()
+        textField.text = "500"
+        textField.font = .systemFont(ofSize: 16)
+        return textField
     }()
+    //    private let rejectButton: UIButton = {
+    //           let button = UIButton(type: .system)
+    //           button.setTitle("Отклонить", for: .normal)
+    //           button.tintColor = .red
+    //           button.backgroundColor = UIColor(red: 0.96, green: 0.97, blue: 0.98, alpha: 1.0)
+    //           button.layer.cornerRadius = 8
+    //           button.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+    //           return button
+    //       }()
+    // MARK: - Initializers
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
@@ -47,11 +66,14 @@ class ExchangeRateTableViewCell: UITableViewCell {
         super.layoutSubviews()
 
         containerView.layer.cornerRadius = 8
+        amountOfPurchaseUSDTextField.layer.cornerRadius = 10
+        amountOfPurchaseUSDTextField.layer.borderWidth = 1
+        amountOfPurchaseUSDTextField.layer.borderColor = UIColor.gray.cgColor
     }
     // MARK: - Setup Views
 
     private func setupViews() {
-        [titleLabel].forEach {
+        [currencyUSDLabel, USDFlagImage, amountOfPurchaseUSDTextField].forEach {
             containerView.addSubview($0)
         }
         contentView.backgroundColor = .systemGray6
@@ -67,10 +89,21 @@ class ExchangeRateTableViewCell: UITableViewCell {
             make.trailing.equalToSuperview().offset(-24)
             make.bottom.equalToSuperview().offset(-8)
         }
-        
-        titleLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(16)
-            make.leading.equalToSuperview().offset(16)
+        USDFlagImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.leading.equalToSuperview().offset(12)
+            make.trailing.equalToSuperview().offset(-301)
+        }
+        currencyUSDLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(18)
+            make.leading.equalToSuperview().offset(58)
+            make.trailing.equalToSuperview().offset(-196)
+        }
+        amountOfPurchaseUSDTextField.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(12)
+            make.leading.equalToSuperview().offset(163)
+            make.trailing.equalToSuperview().offset(-116)
+            make.height.equalTo(32)
         }
     }
 }
