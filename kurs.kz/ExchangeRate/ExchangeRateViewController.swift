abac4de38799595460e096613e251b46c613b217
Akerke <nassyr.akyerke@gmail.com>
2023-07-13 08:47:58 +0600

    [KK-37] Добавляю настройки в VC

--- kurs.kz/ExchangeRate/ExchangeRateViewController.swift
+++ kurs.kz/ExchangeRate/ExchangeRateViewController.swift
@@ -6,24 +6,63 @@
 //
 
 import UIKit
+import SnapKit
 
-class ExchangeRateViewController: UIViewController {
+final class ExchangeRateViewController: UIViewController {
+    
+    // MARK: - UI
 
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.dataSource  = self
+        tableView.delegate = self
+        tableView.register(ExchangeRateTableViewCell.self,
+                           forCellReuseIdentifier: ExchangeRateTableViewCell.reuseIdentifier)
+        tableView.rowHeight = 126
+        tableView.sectionHeaderHeight = 18
+        tableView.separatorStyle = .none
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setupConstraints()
+        
+    }
+    // MARK: - Setup Views
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+        tableView.backgroundColor = .systemGray6
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Setup Constraints
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
-    */
+}
+// MARK: - UITableViewDataSource, UITableViewDelegate
 
+extension ExchangeRateViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ExchangeRateTableViewCell.reuseIdentifier,
+                                                 for: indexPath) as? ExchangeRateTableViewCell
+        return cell ?? UITableViewCell()
+    }
 }
