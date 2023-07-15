f53676543ce4712f0752234f3dd11b45f170ec38
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-16 03:32:11 +0600

    [KK-32] изменил дизайн searchbar, добавил нижний бордер

--- kurs.kz/Currency/CurrencySelectorTableViewHeader.swift
+++ kurs.kz/Currency/CurrencySelectorTableViewHeader.swift
@@ -20,11 +20,18 @@ final class CurrencySelectorTableViewHeader: UITableViewHeaderFooterView {
     private let currencySearchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.setImage(UIImage(named: "search_normal"), for: .search, state: .normal)
-        searchBar.setPositionAdjustment(UIOffset(horizontal: 16, vertical: 0), for: .search)
+        searchBar.searchTextField.font = UIFont.systemFont(ofSize: 14)
+        searchBar.searchTextPositionAdjustment.horizontal = CGFloat(12)
+        searchBar.barTintColor = UIColor.white
+        searchBar.searchTextField.backgroundColor = UIColor.white
+        
         searchBar.placeholder = "Поиск"
         return searchBar
     }()
-    
+    private let lowerBorderView: UIView = {
+        let view = UIView()
+        return view
+    }()
     // MARK: - Initializers
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
@@ -39,14 +46,22 @@ final class CurrencySelectorTableViewHeader: UITableViewHeaderFooterView {
     // MARK: - Setup Views
     private func setupViews() {
         contentView.addSubview(currencySearchBar)
+        contentView.addSubview(lowerBorderView)
         currencySearchBar.layer.borderWidth = 1
         currencySearchBar.layer.borderColor = UIColor.white.cgColor
+        lowerBorderView.backgroundColor = .systemGray6
     }
     
     // MARK: - Setup Constraints
     private func setupConstraints() {
         currencySearchBar.snp.makeConstraints { make in
             make.edges.equalToSuperview()
+        }
+        lowerBorderView.snp.makeConstraints { make in
+            make.top.equalTo(currencySearchBar.snp.bottom)
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(1)
         }
     }
 }
