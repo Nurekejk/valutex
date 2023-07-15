895aa00a502665ea56b92c3b04db45097bbab812
askerbekovbekzhan <askerbekovbekzhan1@gmail.com>
2023-07-15 12:41:01 +0600

    [KK-42] создаю ExchangerScreenViewController

---
+++ kurs.kz/Exchanger Sreen/ExchangerScreenViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ExchangerScreenViewController.swift
+//  kurs.kz
+//
+//  Created by Bekzhan Askerbekov on 15/7/23.
+//
+
+import UIKit
+
+class ExchangerScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
