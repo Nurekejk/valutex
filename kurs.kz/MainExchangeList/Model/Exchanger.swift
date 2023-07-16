ccd69b4a5b6decca756a53f1ed3f3a1f7124cb71
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-16 15:13:07 +0600

    [KK-40] создал модель для ячейки

---
+++ kurs.kz/MainExchangeList/Model/Exchanger.swift
@@ -0,0 +1,21 @@
+//
+//  Exchanger.swift
+//  kurs.kz
+//
+//  Created by MacBook on 16.07.2023.
+//
+
+import Foundation
+
+struct Exchanger {
+    let mainTitle: String
+    let iconImageName: String
+    let rating: Float
+    let totalRatings: Int
+    let address: String
+    let date: String
+    let buyRate: Float
+    let sellRate: Float
+    let distance: String
+}
+
