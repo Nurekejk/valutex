5489ee6bf94f976f04e14158f080b489238bb0fc
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-17 01:06:12 +0600

    [KK-40] создал exchangerArray

--- kurs.kz/MainExchangeList/ExchangeListViewController.swift
+++ kurs.kz/MainExchangeList/ExchangeListViewController.swift
@@ -24,19 +24,54 @@ final class ExchangeListViewController: UIViewController {
         blue: 249.0 / 255.0,
         alpha: 1)
     
-    private let currenciesDictionary = ["Доллар США" : "usd_flag",
-                                        "Евро" : "euro_flag",
-                                        "Рос.рубль" : "ru_flag",
-                                        "Кирг.сом" : "kgs_flag",
-                                        "Кит.юань" : "cn_flag"]
+    private let exchangersArray = [Exchanger(mainTitle: "Som Exchange",
+                                             iconImageName: "som_exchange",
+                                             rating: 4.9,
+                                             totalRatings: 15,
+                                             address: "г.Алматы, ул. Ауэзова 14",
+                                             date: "1 марта, 2023 18:05:33",
+                                             buyRate: 448,
+                                             sellRate: 451,
+                                             distance: "1 км"),
+                                   Exchanger(mainTitle: "МИГ",
+                                             iconImageName: "mig_exchange",
+                                             rating: 4.9,
+                                             totalRatings: 15,
+                                             address: "ул. Толе би, 297 г, уг. ул. Тлендиева",
+                                             date: "1 марта, 2023 18:05:33",
+                                             buyRate: 448.5,
+                                             sellRate: 451,
+                                             distance: "1 км"),
+                                   Exchanger(mainTitle: "Блиц",
+                                             iconImageName: "blank_icon",
+                                             rating: 4.9,
+                                             totalRatings: 15,
+                                             address: "ул. Толе би, 297",
+                                             date: "1 марта, 2023 18:05:33",
+                                             buyRate: 448,
+                                             sellRate: 449.5,
+                                             distance: "5 км"),
+                                   Exchanger(mainTitle: "Limpopo",
+                                             iconImageName: "limpopo_exchange",
+                                             rating: 4.9,
+                                             totalRatings: 15,
+                                             address: "534, пр-т. Сейфуллина уг",
+                                             date: "1 марта, 2023 18:05:33",
+                                             buyRate: 448.1,
+                                             sellRate: 451,
+                                             distance: "3 км"),
+                                   Exchanger(mainTitle: "МИГ",
+                                             iconImageName: "ьшп_exchange",
+                                             rating: 4.9,
+                                             totalRatings: 15,
+                                             address: "мкр. Ақбұлақ ул. Хан шатыр, 273",
+                                             date: "1 марта, 2023 18:05:33",
+                                             buyRate: 448.5,
+                                             sellRate: 451,
+                                             distance: "1 км")]
     
-    private let currenciesKeyArray = ["Доллар США",
-                                      "Евро",
-                                      "Рос.рубль",
-                                      "Кирг.сом",
-                                      "Кит.юань"]
+    private var searchArray = [Exchanger]()
     
-    private var searchKeyArray = [String]()
     private var isSearching = false
     weak var delegate: CurrencySelectorViewControllerDelegate?
     
@@ -51,37 +86,37 @@ final class ExchangeListViewController: UIViewController {
     private lazy var exitButton: UIButton = {
         let button = UIButton(type: .system)
         button.setImage(UIImage(named: "cross"), for: .normal)
-        button.addTarget(self, action: #selector(closeController), for: .touchUpInside)
+//        button.addTarget(self, action: #selector(closeController), for: .touchUpInside)
         return button
     }()
 
     private lazy var selectButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Выбрать", for: .normal)
-        button.addTarget(self, action: #selector(currencyDidSelect), for: .touchUpInside)
+//        button.addTarget(self, action: #selector(currencyDidSelect), for: .touchUpInside)
         button.titleLabel?.font = .systemFont(ofSize: 16)
         button.setTitleColor(.white, for: .normal)
         return button
     }()
     
-    private lazy var headerView: CurrencySelectorTableViewHeader = {
-        let headerView = CurrencySelectorTableViewHeader()
-        headerView.getSearchBar().delegate = self
-        return headerView
-    }()
-    
+//    private lazy var headerView: CurrencySelectorTableViewHeader = {
+//        let headerView = CurrencySelectorTableViewHeader()
+//        headerView.getSearchBar().delegate = self
+//        return headerView
+//    }()
+//    exchangeListTableView
     private lazy var exchangeListTableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.dataSource = self
         tableView.delegate = self
         
         tableView.separatorStyle = .none
+//        tableView.register(ExchangeListTableViewCell.self,
+//                           forHeaderFooterViewReuseIdentifier: ExchangeListTableViewCell.identifier)
         tableView.register(ExchangeListTableViewCell.self,
-                           forHeaderFooterViewReuseIdentifier: ExchangeListTableViewCell.identifier)
-//        tableView.register(CurrencySelectorTableViewCell.self,
-//                           forCellReuseIdentifier: CurrencySelectorTableViewCell.identifier)
-        tableView.rowHeight = 56
-        tableView.tableHeaderView = headerView
+                           forCellReuseIdentifier: ExchangeListTableViewCell.identifier)
+        tableView.rowHeight = 87
+//        tableView.tableHeaderView = headerView
         
         return tableView
     }()
@@ -95,7 +130,7 @@ final class ExchangeListViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        currenciesTableView.layer.cornerRadius = 8
+        exchangeListTableView.layer.cornerRadius = 8
         selectButton.backgroundColor = buttonBlueColor
         selectButton.layer.cornerRadius = 12
 
@@ -103,7 +138,7 @@ final class ExchangeListViewController: UIViewController {
     
     // MARK: - Setup Views
     private func setupViews() {
-        view.addSubview(currenciesTableView)
+        view.addSubview(exchangeListTableView)
         view.addSubview(chooseCurrencyLabel)
         view.addSubview(exitButton)
         view.addSubview(selectButton)
@@ -114,109 +149,114 @@ final class ExchangeListViewController: UIViewController {
     private func setupConstraints() {
         
         let tableWidth = UIScreen.main.bounds.width - 32
-        headerView.frame = CGRect(x: 0, y: 0, width: tableWidth, height: 52)
+//        headerView.frame = CGRect(x: 0, y: 0, width: tableWidth, height: 52)
         
-        chooseCurrencyLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(28)
-            make.leading.equalToSuperview().offset(16)
-            make.height.equalTo(24)
-        }
-        exitButton.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(28)
-            make.trailing.equalToSuperview().offset(-16)
-            make.size.equalTo(24)
-        }
-        currenciesTableView.snp.makeConstraints { make in
+//        chooseCurrencyLabel.snp.makeConstraints { make in
+//            make.top.equalToSuperview().offset(28)
+//            make.leading.equalToSuperview().offset(16)
+//            make.height.equalTo(24)
+//        }
+//        exitButton.snp.makeConstraints { make in
+//            make.top.equalToSuperview().offset(28)
+//            make.trailing.equalToSuperview().offset(-16)
+//            make.size.equalTo(24)
+//        }
+        exchangeListTableView.snp.makeConstraints { make in
             make.top.equalTo(chooseCurrencyLabel.snp.bottom).offset(16)
             make.width.equalTo(tableWidth)
-            make.height.equalTo(328)
+            make.bottom.equalToSuperview()
             make.leading.equalToSuperview().offset(16)
         }
-        selectButton.snp.makeConstraints { make in
-            make.top.equalTo(currenciesTableView.snp.bottom).offset(16)
-            make.leading.equalToSuperview().offset(16)
-            make.width.equalTo(343)
-            make.height.equalTo(52)
-        }
+//        selectButton.snp.makeConstraints { make in
+//            make.top.equalTo(currenciesTableView.snp.bottom).offset(16)
+//            make.leading.equalToSuperview().offset(16)
+//            make.width.equalTo(343)
+//            make.height.equalTo(52)
+//        }
     }
     
     // MARK: - Action
-    @objc func closeController() {
-        dismiss(animated: true, completion: nil)
-    }
-    @objc func currencyDidSelect() {
-        if let selectedIndexPath = currenciesTableView.indexPathForSelectedRow,
-        let senderViewController = delegate {
-            senderViewController.currencyDidSelect(selectedIndexPath:
-                                                    selectedIndexPath,
-                                                   isSearching: isSearching,
-                                                   searchArray: searchKeyArray)
-            dismiss(animated: true, completion: nil)
-        }
-    }
+//    @objc func currencyDidSelect() {
+//        if let selectedIndexPath = exchangeListTableView.indexPathForSelectedRow,
+//        let senderViewController = delegate {
+//            senderViewController.currencyDidSelect(selectedIndexPath:
+//                                                    selectedIndexPath,
+//                                                   isSearching: isSearching,
+//                                                   searchArray: searchArray)
+//            dismiss(animated: true, completion: nil)
+//        }
+//    }
 }
 
     // MARK: - UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate
 extension ExchangeListViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        isSearching ? searchKeyArray.count : currenciesKeyArray.count
+        isSearching ? searchArray.count : exchangersArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if let cell = tableView.dequeueReusableCell(withIdentifier: ExchangeListTableViewCell.identifier,
-                                                    for: indexPath) as? ExchangeListTableViewCell {
-            let dictionaryKey = isSearching ?
-            searchKeyArray[indexPath.row] : currenciesKeyArray[indexPath.row]
-            if let flagName = currenciesDictionary[dictionaryKey] {
-                cell.configureCell(withValue: dictionaryKey, named: flagName)
-                let customSelectionView = UIView()
-                customSelectionView.backgroundColor = UIColor.white
-                cell.selectedBackgroundView = customSelectionView
-                return cell
-            } else {
-                return UITableViewCell()
-            }
-        } else {
-            return UITableViewCell()
-        }
+        
+       if let cell = tableView.dequeueReusableCell(withIdentifier:
+                                                    ExchangeListTableViewCell.identifier,
+                                                   for: indexPath) as? ExchangeListTableViewCell {
+           cell.changeExchanger(with: exchangersArray[indexPath.row])
+           return cell
+       } else {
+           return UITableViewCell()
+       }
+//            let dictionaryKey = isSearching ?
+//            searchArray[indexPath.row] : exchangersArray[indexPath.row]
+//            if let flagName = currenciesDictionary[dictionaryKey] {
+//                cell.configureCell(withValue: dictionaryKey, named: flagName)
+//                let customSelectionView = UIView()
+//                customSelectionView.backgroundColor = UIColor.white
+//                cell.selectedBackgroundView = customSelectionView
+//                return cell
+//            } else {
+//                return UITableViewCell()
+//            }
+//        } else {
+//            return UITableViewCell()
+//        }
+        
     }
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.isEmpty {
-            isSearching = false
-            currenciesTableView.reloadData()
-            searchBar.resignFirstResponder()
-        } else {
-            isSearching = true
-            searchKeyArray = currenciesKeyArray.filter { currency in
-                return currency.localizedCaseInsensitiveContains(searchText)
-            }
-            currenciesTableView.reloadData()
-        }
+//        if searchText.isEmpty {
+//            isSearching = false
+//            exchangeListTableView.reloadData()
+//            searchBar.resignFirstResponder()
+//        } else {
+//            isSearching = true
+//            searchKeyArray = currenciesKeyArray.filter { currency in
+//                return currency.localizedCaseInsensitiveContains(searchText)
+//            }
+//            exchangeListTableView.reloadData()
+//        }
     }
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        isSearching = false
-        searchKeyArray.removeAll()
-        searchBar.text = ""
-        searchBar.resignFirstResponder()
+//        isSearching = false
+//        searchKeyArray.removeAll()
+//        searchBar.text = ""
+//        searchBar.resignFirstResponder()
     }
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
     }
 }
 // MARK: - PanModalPresentable
-extension ExchangeListViewController: PanModalPresentable {
-
-    var panScrollable: UIScrollView? {
-        return nil
-    }
-    var shortFormHeight: PanModalHeight {
-        return .contentHeight(496)
-    }
-    var longFormHeight: PanModalHeight {
-        return .maxHeightWithTopInset(40)
-    }
-}
+// extension ExchangeListViewController: PanModalPresentable {
+//
+//    var panScrollable: UIScrollView? {
+//        return nil
+//    }
+//    var shortFormHeight: PanModalHeight {
+//        return .contentHeight(496)
+//    }
+//    var longFormHeight: PanModalHeight {
+//        return .maxHeightWithTopInset(40)
+//    }
+// }
 // MARK: - Protocol
 protocol CurrencySelectorViewControllerDelegate: AnyObject {
     func currencyDidSelect(selectedIndexPath: IndexPath,
