7373407d9162dfffa07eae26ddd0052dfddb489f
abitoffka <aigerim.abitayevaa@gmail.com>
2023-07-16 21:02:54 +0600

    [KK-50] Создала custom AcceptedSendTableViewCell

--- kurs.kz/PartnerOffer/AcceptedSendTableViewCell.swift
+++ kurs.kz/PartnerOffer/AcceptedSendTableViewCell.swift
@@ -6,13 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
-class AcceptedSendTableViewCell: UITableViewCell {
+final class AcceptedSendTableViewCell: UITableViewCell {
     
     // MARK: - State
     static let reuseID = String(describing: AcceptedSendTableViewCell.self)
     
     // MARK: - UI
+    private lazy var acceptedSendImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "accepted-image")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private lazy var acceptedSendCellStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 16
+        return stackView
+    }()
+    
+    private lazy var acceptedSendLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Принятые (2)"
+        label.textColor = UIColor(named: "partnerOfferBasicTextColor")
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        return label
+    }()
+    
+    private lazy var arrowRightImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "arrow-right")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        return imageView
+    }()
     
     // MARK: - Initializers
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -27,11 +60,35 @@ class AcceptedSendTableViewCell: UITableViewCell {
     
     // MARK: - Setup Views
     private func setupViews() {
+        contentView.addSubview(acceptedSendCellStackView)
         
+        [acceptedSendImageView, acceptedSendLabel, arrowRightImageView].forEach {
+            acceptedSendCellStackView.addArrangedSubview($0)
+        }
     }
     
     // MARK: - Setup Constraints
     private func setupConstraints() {
+        acceptedSendCellStackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
+        }
         
+        acceptedSendImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.height.equalTo(48)
+            make.width.equalTo(50)
+        }
+        
+        acceptedSendLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+        }
+        
+        arrowRightImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.height.equalTo(16)
+            make.width.equalTo(16)
+        }
     }
 }
--- kurs.kz/PartnerOffer/PartnerOfferViewController.swift
+++ kurs.kz/PartnerOffer/PartnerOfferViewController.swift
@@ -12,10 +12,12 @@ class PartnerOfferViewController: UIViewController {
     // MARK: - UI
     private lazy var acceptedSendTableView: UITableView = {
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self,
-                           forCellReuseIdentifier: "cell")
+        tableView.register(AcceptedSendTableViewCell.self,
+                           forCellReuseIdentifier: AcceptedSendTableViewCell.reuseID)
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.rowHeight = 80.0
+        tableView.tableHeaderView?.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
     
@@ -28,6 +30,11 @@ class PartnerOfferViewController: UIViewController {
         setupNavigationBar()
     }
     
+    override func viewDidLayoutSubviews() {
+        acceptedSendTableView.layer.cornerRadius = 8
+        acceptedSendTableView.layer.masksToBounds = true
+    }
+    
     // MARK: - Setup Navigation Bar
     private func setupNavigationBar() {
         edgesForExtendedLayout = []
@@ -36,7 +43,7 @@ class PartnerOfferViewController: UIViewController {
     
     // MARK: - Setup Views
     private func setupViews() {
-        view.backgroundColor = .white
+        view.backgroundColor = .systemGray6
         
         [acceptedSendTableView].forEach {
             view.addSubview($0)
@@ -49,23 +56,27 @@ class PartnerOfferViewController: UIViewController {
             make.top.equalToSuperview().offset(16)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-24)
-            make.height.equalTo(176)
+            make.bottom.equalToSuperview()
         }
     }
 }
 
 // MARK: - UITableViewDataSource, UITableViewDelegate
 extension PartnerOfferViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
-            for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: AcceptedSendTableViewCell.reuseID,
+                                                       for: indexPath) as? AcceptedSendTableViewCell else {
+            fatalError("Could not cast to AcceptedSendTableViewCell")
+        }
         cell.selectionStyle = .none
         return cell
     }
-    
-    
 }
