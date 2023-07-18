93172a518f837b2eca3f79d07673713671093c7e
Akerke <nassyr.akyerke@gmail.com>
2023-07-18 09:52:39 +0600

    [KK-37] Добавила headerView

--- kurs.kz/ExchangeRate/ExchangeRateTableViewHeaderView.swift
+++ kurs.kz/ExchangeRate/ExchangeRateTableViewHeaderView.swift
@@ -9,12 +9,53 @@ import UIKit
 
 class ExchangeRateTableViewHeaderView: UITableViewHeaderFooterView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    // MARK: - UI
 
-}
+    private lazy var purchaseLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Покупка"
+        label.font = .systemFont(ofSize: 10)
+        return label
+    }()
+    private lazy var sellLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Продажа"
+        label.font = .systemFont(ofSize: 10)
+        return label
+    }()
+    // MARK: - LifeCycle
+
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+
+        setupViews()
+        setupConstriats()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup Views
+
+    private func setupViews() {
+        [purchaseLabel, sellLabel].forEach {
+            contentView.addSubview($0)
+        }
+        contentView.backgroundColor = .systemGray6
+    }
+        // MARK: - Setup Constraits
+
+        func setupConstriats() {
+
+            purchaseLabel.snp.makeConstraints { make in
+                make.top.equalToSuperview().offset(12)
+                make.trailing.equalToSuperview().offset(-33)
+            }
+
+            sellLabel.snp.makeConstraints { make in
+                make.top.equalToSuperview().offset(12)
+                make.trailing.equalToSuperview().offset(-16)
+            }
+        }
+    }
