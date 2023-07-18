139e0d67158a30cab31103aa595dbdc472ef470c
Akerke <nassyr.akyerke@gmail.com>
2023-07-18 08:38:04 +0600

    [KK-37] Удалила cases и перезадала UI элементы

--- kurs.kz/ExchangeRate/ExchangeRateViewController.swift
+++ kurs.kz/ExchangeRate/ExchangeRateViewController.swift
@@ -12,14 +12,17 @@ final class ExchangeRateViewController: UIViewController {
 
     // MARK: - State
 
-    // 1 trying
-    //    let image: [UIImage] = [
-    //        UIImage(named: "USDflag").flatMap { $0 },
-    //        UIImage(named: "EUROflag").flatMap { $0 },
-    //        UIImage(named: "RUflag").flatMap { $0 }
-    //    ].compactMap { $0 }
+    private let flagImage: [String] = ["USDflag", "EUROflag", "RUflag"]
+
+       private let currencyLabel: [String] = ["Доллар", "Евро", "Рос. рубль"]
 
     // MARK: - UI
+
+    private lazy var headerView: ExchangeRateTableViewHeaderView = {
+            let headerView = ExchangeRateTableViewHeaderView()
+
+            return headerView
+        }()
 
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
@@ -28,10 +31,23 @@ final class ExchangeRateViewController: UIViewController {
         tableView.delegate = self
         tableView.register(ExchangeRateTableViewCell.self,
                            forCellReuseIdentifier: ExchangeRateTableViewCell.reuseIdentifier)
+        tableView.register(ExchangeRateTableViewCell.self,
+                           forHeaderFooterViewReuseIdentifier: ExchangeRateTableViewCell.reuseIdentifier)
+        tableView.register(ExchangeRateTableViewCell.self,
+                           forHeaderFooterViewReuseIdentifier: ExchangeRateTableViewCell.reuseIdentifier)
+        tableView.tableHeaderView = headerView
+               tableView.tableFooterView = footerView
         tableView.rowHeight = 80
         tableView.separatorStyle = .none
         return tableView
     }()
+
+    private lazy var footerView: ExchangeRateTableViewFooterView = {
+            let footerView = ExchangeTableViewFooterView()
+
+            return footerView
+        }()
+
     
     // MARK: - Lifecycle
     
@@ -56,6 +72,9 @@ final class ExchangeRateViewController: UIViewController {
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
+        headerView.frame = CGRect(x: 0, y: 0, width: 311, height: 36)
+
+               footerView.frame = CGRect(x: 0, y: 0, width: 311, height: 34)
     }
 }
 
@@ -63,7 +82,7 @@ final class ExchangeRateViewController: UIViewController {
 
 extension ExchangeRateViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return UIView() // Create an empty view as a placeholder for the spacing
+        return UIView()
     }
     func tableView(_ tableView: UITableView,
                    willDisplayHeaderView view: UIView,
@@ -71,45 +90,49 @@ extension ExchangeRateViewController: UITableViewDataSource, UITableViewDelegate
         view.backgroundColor = .clear
     }
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return flagImage.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ExchangeRateTableViewCell.reuseIdentifier,
                                                  for: indexPath) as? ExchangeRateTableViewCell
-        switch indexPath.row {
-        case 0:
-            cell!.flagImage.image = UIImage(named: "USDflag")
-            cell!.currencyLabel.text = "Доллар"
-            cell!.amountOfPurchaseTextField.placeholder = "500"
-            cell!.amountOfSaleTextField.placeholder = "500"
-            cell!.trashButton.setImage(UIImage(named: "trash"), for: .normal)
-
-        case 1:
-            cell!.flagImage.image = UIImage(named: "EUROflag")
-            cell!.currencyLabel.text = "Евро"
-            cell!.amountOfPurchaseTextField.placeholder = "500"
-            cell!.amountOfSaleTextField.placeholder = "500"
-            cell!.trashButton.setImage(UIImage(named: "trash"), for: .normal)
-
-        case 2:
-            cell!.flagImage.image = UIImage(named: "RUflag")
-            cell!.currencyLabel.text = "Рубль"
-            cell!.amountOfPurchaseTextField.placeholder = "500"
-            cell!.amountOfSaleTextField.placeholder = "500"
-            cell!.trashButton.setImage(UIImage(named: "trash"), for: .normal)
-
-        default:
-            break
-        }
+        let flagImage = flagImage[indexPath.row]
+        let currencyLabel = currencyLabel[indexPath.row]
+        //        switch indexPath.row {
+        //        case 0:
+        //            cell!.flagImage.image = UIImage(named: "USDflag")
+        //            cell!.currencyLabel.text = "Доллар"
+        //            cell!.amountOfPurchaseTextField.placeholder = "500"
+        //            cell!.amountOfSaleTextField.placeholder = "500"
+        //            cell!.trashButton.setImage(UIImage(named: "trash"), for: .normal)
+        //
+        //        case 1:
+        //            cell!.flagImage.image = UIImage(named: "EUROflag")
+        //            cell!.currencyLabel.text = "Евро"
+        //            cell!.amountOfPurchaseTextField.placeholder = "500"
+        //            cell!.amountOfSaleTextField.placeholder = "500"
+        //            cell!.trashButton.setImage(UIImage(named: "trash"), for: .normal)
+        //
+        //        case 2:
+        //            cell!.flagImage.image = UIImage(named: "RUflag")
+        //            cell!.currencyLabel.text = "Рубль"
+        //            cell!.amountOfPurchaseTextField.placeholder = "500"
+        //            cell!.amountOfSaleTextField.placeholder = "500"
+        //            cell!.trashButton.setImage(UIImage(named: "trash"), for: .normal)
+        //
+        //        default:
+        //            break
+        //        }
         // 2 trying
         //        let image = self.image[indexPath.row]
         //        cell!.configure(with: image, and: indexPath.row.description)
-
+        cell?.configureCell(flagImage: flagImage, currencyLabel: currencyLabel)
         return cell ?? UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
     }
 }
