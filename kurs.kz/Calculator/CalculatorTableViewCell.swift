e937a950367fde1a852476ebc3823d4d087b29fa
Akerke <nassyr.akyerke@gmail.com>
2023-07-19 10:56:28 +0600

    [KK-48] Настроила UI элементы

--- kurs.kz/Calculator/CalculatorTableViewCell.swift
+++ kurs.kz/Calculator/CalculatorTableViewCell.swift
@@ -28,7 +28,7 @@ final class CalculatorTableViewCell: UITableViewCell {
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = "MИГ"
-        label.font = .boldSystemFont(ofSize: 16)
+        label.font = .boldSystemFont(ofSize: 14)
         return label
     }()
     private lazy var starImage: UIImageView = {
@@ -40,25 +40,26 @@ final class CalculatorTableViewCell: UITableViewCell {
     private lazy var rateLabel: UILabel = {
         let label = UILabel()
         label.text = "4,9 (15)"
-        label.font = .boldSystemFont(ofSize: 16)
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = UIColor.lightGray
         return label
     }()
     private lazy var addressLabel: UILabel = {
         let label = UILabel()
         label.text = "ул. Толе Би, 297 г, уг. ул. Тлендиева"
-        label.font = .boldSystemFont(ofSize: 16)
+        label.font = .systemFont(ofSize: 12)
         return label
     }()
     private lazy var kmLabel: UILabel = {
         let label = UILabel()
         label.text = "1 км"
-        label.font = .boldSystemFont(ofSize: 16)
+        label.font = .systemFont(ofSize: 10)
         return label
     }()
     private lazy var dateLabel: UILabel = {
         let label = UILabel()
         label.text = "1 октября, 2023 18:00:00"
-        label.font = .systemFont(ofSize: 14)
+        label.font = .systemFont(ofSize: 12)
         label.textColor = UIColor.lightGray
         label.textAlignment = .left
         return label
@@ -70,81 +71,79 @@ final class CalculatorTableViewCell: UITableViewCell {
         return label
     }()
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-           super.init(style: style, reuseIdentifier: reuseIdentifier)
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
 
-           setupViews()
-           setupConstraints()
+        setupViews()
+        setupConstraints()
 
-       }
-       required init?(coder: NSCoder) {
-           fatalError("init(coder:) has not been implemented")
-       }
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
-       override func layoutSubviews() {
-           super.layoutSubviews()
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.layer.cornerRadius = 8
+    }
 
-           containerView.layer.cornerRadius = 8
-       }
+    // MARK: - Setup Views
 
-       // MARK: - Setup Views
+    private func setupViews() {
+        [migLogoImage, titleLabel, starImage, rateLabel,
+         addressLabel, kmLabel, dateLabel,amountLabel].forEach {
+            containerView.addSubview($0)
+        }
+        let backgroundGrayColor = UIColor(
+            red: 246.0 / 255.0,
+            green: 247.0 / 255.0,
+            blue: 249.0 / 255.0,
+            alpha: 1)
 
-       private func setupViews() {
-           [migLogoImage, titleLabel, starImage, rateLabel,
-            addressLabel, kmLabel, dateLabel,amountLabel].forEach {
-               containerView.addSubview($0)
-           }
+        contentView.backgroundColor = backgroundGrayColor
+        containerView.backgroundColor = .white
+        contentView.addSubview(containerView)
+    }
 
-           let backgroundGrayColor = UIColor(
-               red: 246.0 / 255.0,
-               green: 247.0 / 255.0,
-               blue: 249.0 / 255.0,
-               alpha: 1)
+    // MARK: - Setup Constraints
 
-           contentView.backgroundColor = backgroundGrayColor
-           containerView.backgroundColor = .white
-           contentView.addSubview(containerView)
-       }
-
-       // MARK: - Setup Constraints
-
-       private func setupConstraints() {
-           containerView.snp.makeConstraints { make in
-               make.top.equalToSuperview().offset(8)
-               make.leading.equalToSuperview().offset(24)
-               make.trailing.equalToSuperview().offset(-24)
-               make.bottom.equalToSuperview().offset(-8)
-           }
-           migLogoImage.snp.makeConstraints { make in
-               make.top.equalToSuperview().offset(17.5)
-               make.leading.equalToSuperview().offset(12)
-           }
-           titleLabel.snp.makeConstraints { make in
-               make.top.equalTo(titleLabel.snp.bottom).offset(12)
-               make.leading.equalTo(migLogoImage.snp.trailing).offset(16)
-           }
-           starImage.snp.makeConstraints { make in
-               make.top.equalToSuperview().offset(15)
-               make.leading.equalTo(titleLabel.snp.trailing).offset(8)
-           }
-           rateLabel.snp.makeConstraints { make in
-               make.top.equalToSuperview().offset(13.5)
-               make.leading.equalTo(starImage.snp.trailing).offset(4)
-           }
-           addressLabel.snp.makeConstraints { make in
-               make.top.equalTo(titleLabel.snp.bottom).offset(2)
-               make.leading.equalTo(migLogoImage.snp.trailing).offset(16)
-           }
-           kmLabel.snp.makeConstraints { make in
-               make.top.equalToSuperview().offset(33.5)
-               make.leading.equalTo(addressLabel.snp.trailing).offset(8)
-           }
-           dateLabel.snp.makeConstraints { make in
-               make.top.equalTo(kmLabel.snp.bottom).offset(8)
-               make.leading.equalToSuperview().offset(12)
-           }
-           amountLabel.snp.makeConstraints { make in
-               make.top.equalTo(kmLabel.snp.bottom).offset(8)
-               make.trailing.equalToSuperview().offset(-12)
-           }
-       }
-   }
+    private func setupConstraints() {
+        containerView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(4)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+            make.bottom.equalToSuperview().offset(-4)
+        }
+        migLogoImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(17.5)
+            make.leading.equalToSuperview().offset(12)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(12)
+            make.leading.equalTo(migLogoImage.snp.trailing).offset(16)
+        }
+        starImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(15)
+            make.leading.equalTo(titleLabel.snp.trailing).offset(8)
+        }
+        rateLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(13.5)
+            make.leading.equalTo(starImage.snp.trailing).offset(4)
+        }
+        addressLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(2)
+            make.leading.equalTo(migLogoImage.snp.trailing).offset(16)
+        }
+        kmLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(33.5)
+            make.trailing.equalToSuperview().offset(-44)
+        }
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(kmLabel.snp.bottom).offset(8)
+            make.leading.equalToSuperview().offset(12)
+        }
+        amountLabel.snp.makeConstraints { make in
+            make.top.equalTo(kmLabel.snp.bottom).offset(8)
+            make.trailing.equalToSuperview().offset(-12)
+        }
+    }
+}
