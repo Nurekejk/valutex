7ae130b8da09f187dfdf97af55c33040b0ecc995
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-20 15:24:11 +0600

    [KK-56] создал структуру Currency и добавил codingkeys

--- kurs.kz/ClientOffer/Model/Currency.swift
+++ kurs.kz/ClientOffer/Model/Currency.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct OfferSellCurrency: Decodable {
+    let flag: String
+    let russianName: String
+    let symbol: String
+    let code: String
+    let kazakhName: String
+    let englishName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case flag
+        case symbol
+        case code
+        case russianName = "ru_name"
+        case kazakhName = "kz_name"
+        case englishName = "eng_name"
+    }
+}
