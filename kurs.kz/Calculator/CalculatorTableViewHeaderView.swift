1afc42b26240174bdaae237162c37c775072154a
Akerke <nassyr.akyerke@gmail.com>
2023-07-20 14:29:34 +0600

    [KK-48] Добавила constraints

--- kurs.kz/Calculator/CalculatorTableViewHeaderView.swift
+++ kurs.kz/Calculator/CalculatorTableViewHeaderView.swift
@@ -6,88 +6,114 @@
 //
 
 import UIKit
+import SnapKit
 
 final class CalculatorTableViewHeaderView: UITableViewHeaderFooterView {
 
     // MARK: - UI
 
-        private lazy var purchaseLabel: UILabel = {
-            let label = UILabel()
-            label.text = "Покупка"
-            label.textColor = .lightGray
-            label.font = .systemFont(ofSize: 10)
-            return label
-        }()
-        private lazy var arrowUpButton: UIButton = {
-            let button = UIButton(type: .system)
-            let iconImage = UIImage(named: "arrow-up")
-            button.setImage(iconImage, for: .normal)
-            button.addTarget(self, action: #selector(arrowUpButtonDidPressed),
-                             for: .touchUpInside)
-            return button
-        }()
-        private lazy var sellLabel: UILabel = {
-            let label = UILabel()
-            label.text = "Продажа"
-            label.textColor = .lightGray
-            label.font = .systemFont(ofSize: 10)
-            return label
-        }()
-        private lazy var arrowDownButton: UIButton = {
-            let button = UIButton(type: .system)
-            let iconImage = UIImage(named: "arrow-down")
-            button.setImage(iconImage, for: .normal)
-            button.addTarget(self, action: #selector(arrowDownButtonDidPressed),
-                             for: .touchUpInside)
-            return button
-        }()
+    private lazy var flagImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "KZTflag")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private lazy var currencyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "KZT"
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+    private lazy var dropDownButton: UIButton = {
+        let button = UIButton(type: .system)
+        let iconImage = UIImage(named: "drop-down")
+        button.setImage(iconImage, for: .normal)
+        button.addTarget(self, action: #selector(dropDownButtonDidPressed),
+                         for: .touchUpInside)
+        return button
+    }()
+    private lazy var separatorLineImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "separator-line")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    private lazy var arrowLeftRightButton: UIButton = {
+        let button = UIButton(type: .system)
+        let iconImage = UIImage(named: "arrow-left-right")
+        button.setImage(iconImage, for: .normal)
+        button.addTarget(self, action: #selector(arrowLeftRightButtonDidPressed),
+                         for: .touchUpInside)
+        return button
+    }()
 
-        // MARK: - LifeCycle
+    // MARK: - LifeCycle
 
-        override init(reuseIdentifier: String?) {
-            super.init(reuseIdentifier: reuseIdentifier)
-            setupViews()
-            setupConstraints()
-        }
-        required init?(coder: NSCoder) {
-            fatalError("init(coder:) has not been implemented")
-        }
-
-        // MARK: - Setup Views
-
-        private func setupViews() {
-            [purchaseLabel, sellLabel, arrowUpButton, arrowDownButton].forEach {
-                contentView.addSubview($0)
-            }
-            contentView.backgroundColor = .white
-        }
-        // MARK: - Setup Constraints
-
-        func setupConstraints() {
-            arrowDownButton.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(12)
-                make.trailing.equalToSuperview().offset(-5)
-            }
-            sellLabel.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(12)
-                make.trailing.equalTo(arrowDownButton.snp.leading).offset(-4)
-            }
-            arrowUpButton.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(12)
-                make.trailing.equalTo(sellLabel.snp.leading).offset(-17.5)
-            }
-            purchaseLabel.snp.makeConstraints { make in
-                make.top.equalToSuperview().offset(12)
-                make.trailing.equalTo(arrowUpButton.snp.leading).offset(-4)
-            }
-        }
-
-        // MARK: - Actions
-
-        @objc private func arrowUpButtonDidPressed() {
-
-        }
-        @objc private func arrowDownButtonDidPressed() {
-
-        }
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        setupViews()
+        setupConstraints()
     }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Setup Views
+
+    private func setupViews() {
+        [flagImage, currencyLabel, dropDownButton, separatorLineImage, arrowLeftRightButton].forEach {
+            contentView.addSubview($0)
+        }
+        contentView.backgroundColor = .white
+    }
+    // MARK: - Setup Constraints
+
+    func setupConstraints() {
+        flagImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().offset(16)
+        }
+        currencyLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(14)
+            make.leading.equalTo(flagImage.snp.trailing).offset(16)
+        }
+        dropDownButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(21.54)
+            make.leading.equalTo(dropDownButton.snp.trailing).offset(16)
+        }
+        separatorLineImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(8)
+            make.leading.equalTo(dropDownButton.snp.trailing).offset(33.5)
+        }
+        arrowLeftRightButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalTo(separatorLineImage.snp.trailing).offset(12)
+        }
+        separatorLineImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(8)
+            make.leading.equalTo(arrowLeftRightButton.snp.trailing).offset(12)
+        }
+        dropDownButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(21.54)
+            make.leading.equalToSuperview().offset(18.5)
+        }
+        currencyLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(14)
+            make.trailing.equalTo(dropDownButton.snp.leading).offset(-16)
+        }
+        flagImage.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.trailing.equalTo(currencyLabel.snp.leading).offset(-16)
+        }
+
+    }
+
+    // MARK: - Actions
+
+    @objc private func dropDownButtonDidPressed() {
+
+    }
+    @objc private func arrowLeftRightButtonDidPressed() {
+
+    }
+}
