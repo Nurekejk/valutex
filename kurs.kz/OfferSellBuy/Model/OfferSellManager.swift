139ca34e160b4a2f5007716100d4c4aa6d9379af
Adil Bazylov <adil.bazylov@gmail.com>
2023-07-20 19:18:12 +0600

    [KK-56] создал функцию parseJSON и привел код в порядок

--- kurs.kz/OfferSellBuy/Model/OfferSellManager.swift
+++ kurs.kz/OfferSellBuy/Model/OfferSellManager.swift
@@ -9,47 +9,38 @@ import Foundation
 
 struct OfferSellManager {
     let currencyListURL = "http://77.240.38.143:4443/currencies_list"
-    var OfferSellManagerDelegate: OfferSellManagerDelegate
+    var OfferSellManagerDelegate: OfferSellManagerDelegate?
     
     private func fetchCurrencies() {
         let urlsesion = URLSession.shared
         if let url = URL(string: currencyListURL) {
             var urlRequest = URLRequest(url: url)
             urlRequest.httpMethod = "GET"
-
+            
             let task = urlsesion.dataTask(with: urlRequest) { data, _, error in
-                if let data = data {
-                    do {
-                        
+                if error != nil {
+                    self.OfferSellManagerDelegate?.didFailWithError(error!)
+                    return
+                }
+                if let safeData = data {
+                    if let currency = parseJSON(currencyData: safeData) {
                         DispatchQueue.main.async {
-                            self.OfferSellManagerDelegate.currencyDidUpdate(<#T##currency: OfferSellCurrency##OfferSellCurrency#>)
+                            self.OfferSellManagerDelegate?.currencyDidUpdate(currency)
                         }
-
-                    } catch let error {
-                        print(error)
                     }
-
-                } else if let error = error {
-                    print("HTTP Request Failed \(error)")
                 }
             }
             task.resume()
         }
-    } else {
-        OfferSellManagerDelegate.didFailWithError(error)
     }
     
     func parseJSON(currencyData:Data) -> OfferSellCurrency? {
         let decoder = JSONDecoder()
         do {
-          let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            let id = decodedData.weather[0].id
-            let temp = decodedData.main.temp
-            let name = decodedData.name
-            let weatherModel = cu
-            return weatherModel
+            let decodedData = try decoder.decode(OfferSellCurrency.self, from: currencyData)
+            return decodedData
         } catch {
-            weatherDelegate?.didFailWithError(error)
+            OfferSellManagerDelegate?.didFailWithError(error)
             return nil
         }
     }
