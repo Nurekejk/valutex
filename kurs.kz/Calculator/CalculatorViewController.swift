c6ed8e152be4f132fd5f54f81030c78b6458d7f6
Akerke <nassyr.akyerke@gmail.com>
2023-07-20 14:29:57 +0600

    [KK-48] Добавила switch

--- kurs.kz/Calculator/CalculatorViewController.swift
+++ kurs.kz/Calculator/CalculatorViewController.swift
@@ -6,17 +6,24 @@
 //
 
 import UIKit
+import SnapKit
 
-class CalculatorViewController: UIViewController {
+final class CalculatorViewController: UIViewController {
 
     // MARK: - UI
 
+    private lazy var headerView: CalculatorTableViewHeaderView = {
+        let headerView = CalculatorTableViewHeaderView()
+        headerView.backgroundColor = .systemGray6
+        return headerView
+    }()
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.dataSource  = self
         tableView.delegate = self
         tableView.register(CalculatorTableViewCell.self,
                            forCellReuseIdentifier: CalculatorTableViewCell.reuseIdentifier)
+        tableView.tableHeaderView = headerView
         tableView.rowHeight = 87
         //        tableView.sectionHeaderHeight = 18
         tableView.separatorStyle = .none
@@ -26,20 +33,25 @@ class CalculatorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupViews()
         setupConstraints()
     }
     // MARK: - Setup Views
 
     private func setupViews() {
-        view.addSubview(tableView)
         tableView.backgroundColor = .systemGray6
+        view.addSubview(headerView)
+        view.addSubview(tableView)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.layer.cornerRadius = 8
     }
 
     // MARK: - Setup Constraints
 
     private func setupConstraints() {
+        headerView.frame = CGRect(x: 0, y: 0, width: 0, height: 36)
         tableView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
@@ -60,6 +72,19 @@ extension CalculatorViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: CalculatorTableViewCell.reuseIdentifier,
                                                  for: indexPath) as? CalculatorTableViewCell
+        switch indexPath.row {
+        case 0:
+            cell?.configureCell(migLogoImage: UIImage(named: "migLogo"),
+                                titleLabel: "MИГ",
+                                starImage: UIImage(named: "star"),
+                                rateLabel: "4,9 (15)",
+                                addressLabel: "ул. Толе Би, 297 г, уг. ул. Тлендиева",
+                                kmLabel: "1 км",
+                                dateLabel: "1 октября, 2023 18:00:00",
+                                amountLabel: "1000,01")
+        default:
+            break
+        }
         return cell ?? UITableViewCell()
     }
 
