ed75f80061ff000cf5178e0078dc18b76def0433
abitoffka <aigerim.abitayevaa@gmail.com>
2023-07-23 16:57:00 +0600

    [KK-60] Добавила full user model

---
+++ kurs.kz/Registration/Model/FullUser.swift
@@ -0,0 +1,35 @@
+//
+//  FullUser.swift
+//  kurs.kz
+//
+//  Created by Aigerim Abitayeva on 23.07.2023.
+//
+
+import Foundation
+
+struct FullUser: Encodable {
+    let name: String
+    let surname: String
+    let middleName: String
+    let password: String
+    let phone: String
+    let deviceID: String
+    let language: String
+    let currencyCode: String
+    let cityID: Int
+    let smsCode: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case surname
+        case middleName = "middle_name"
+        case password
+        case phone
+        case deviceID = "device_id"
+        case language
+        case currencyCode = "currency_code"
+        case cityID = "city_id"
+        case smsCode = "sms_code"
+    }
+}
+
