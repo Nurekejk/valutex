d84e791e0b8fa78ea4033f16950fa36711c2d7ad
abitoffka <aigerim.abitayevaa@gmail.com>
2023-07-25 15:22:56 +0600

    [KK-60] Обновила User

--- kurs.kz/SignUp/Model/User.swift
+++ kurs.kz/SignUp/Model/User.swift
@@ -8,9 +8,27 @@
 import Foundation
 
 struct User: Codable {
+    let name: String
+    let surname: String
+    let middleName: String
+    let password: String
     let phone: String
+    let deviceID: String
+    let language: String
+    let currencyCode: String
+    let cityID: Int
+    let smsCode: Int
     
     enum CodingKeys: String, CodingKey {
-            case phone
+        case name
+        case surname
+        case middleName = "middle_name"
+        case password
+        case phone
+        case deviceID = "device_id"
+        case language
+        case currencyCode = "currency_code"
+        case cityID = "city_id"
+        case smsCode = "sms_code"
         }
 }
