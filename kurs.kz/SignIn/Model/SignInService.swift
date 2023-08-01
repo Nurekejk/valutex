bb935b0f684d5012884b447c00aadd2795093a3a
Adil Bazylov <adil.bazylov@gmail.com>
2023-08-02 01:43:15 +0600

    [85ztp12f8]  добавил новую функцию с использованием Af

--- kurs.kz/SignIn/Model/SignInService.swift
+++ kurs.kz/SignIn/Model/SignInService.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 final class SignInService {
     private let apiUrlUserLogin = "http://77.240.38.143:4443/login"
@@ -19,7 +20,46 @@ final class SignInService {
     }
 
     // MARK: - Network
-
+    func signIn2(phone: String,
+                 password: String,
+                 completion: @escaping (Result<SignInResponse, NetworkError>) -> Void) {
+        
+        var signIn = URLComponents()
+        signIn.scheme = "http"
+        signIn.host = "77.240.38.143"
+        signIn.port = 4443
+        signIn.path = "/login"
+        
+        guard let url = signIn.url else {
+            print("error while creating URl")
+            return
+        }
+        
+        let headers: HTTPHeaders = [
+            "accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        ]
+        let parameters: Parameters = [
+            "grant_type": "",
+            "username": phone,
+            "password": password,
+            "scope": "",
+            "client_id": "",
+            "client_secret": ""
+        ]
+        
+        AF.request(url, method: .post,
+                   parameters: parameters, headers: headers)
+        .responseDecodable(of: SignInResponse.self) { data in
+            switch data.result {
+            case .success(let value):
+                completion(.success(value))
+            case .failure:
+                completion(.failure(.postRequestError))
+            }
+        }
+    }
+    
     func signIn(phone: String,
                 password: String,
                 completion: @escaping (Result<SignInResponse, NetworkError>) -> Void) {
