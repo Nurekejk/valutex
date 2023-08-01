98d28d8c99449347e29ff7bd6e753f0ca9e53b99
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-01 15:54:15 +0600

    [85ztp14e2] Добавила questionTypeLabel

--- kurs.kz/FAQ/FaqTableViewCell.swift
+++ kurs.kz/FAQ/FaqTableViewCell.swift
@@ -11,5 +11,16 @@ final class FaqTableViewCell: UITableViewCell {
     // MARK: - State
     static let reuseID = String(describing: ProfileTableViewCell.self)
     
+    // MARK: - UI
+    private lazy var questionTypeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Что такое Metalink?"
+        label.textColor = AppColor.gray100.uiColor
+        label.font = AppFont.regular.s14()
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        return label
+    }()
+    
     
 }
