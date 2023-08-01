fd325c6aec734eeeb657dcada7ec138fb5365b20
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-01 15:28:58 +0600

    [85ztp14e2] Добавила questionSearchBar

---
+++ kurs.kz/FAQ/FaqViewController.swift
@@ -0,0 +1,15 @@
+    // MARK: - UI
+    private let questionSearchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.setImage(AppImage.search_normal.uiImage, for: .search, state: .normal)
+        searchBar.searchTextField.font = AppFont.regular.s14()
+        searchBar.searchTextPositionAdjustment.horizontal = CGFloat(12)
+        searchBar.searchTextField.backgroundColor = AppColor.grayWhite.uiColor
+        searchBar.placeholder = "Что ищете?"
+        searchBar.searchBarStyle = .default
+        searchBar.barStyle = .default
+        searchBar.backgroundColor = AppColor.grayWhite.uiColor
+        searchBar.barTintColor = AppColor.grayWhite.uiColor
+        
+        return searchBar
+    }()
