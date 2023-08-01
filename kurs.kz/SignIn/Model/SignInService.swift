37082ffca00c3727331284759d4fc4819a8ad0fc
Adil Bazylov <adil.bazylov@gmail.com>
2023-08-02 01:45:01 +0600

    [85ztp12f8] удалил старую функцию для sign in

--- kurs.kz/SignIn/Model/SignInService.swift
+++ kurs.kz/SignIn/Model/SignInService.swift
@@ -20,9 +20,9 @@ final class SignInService {
     }
 
     // MARK: - Network
-    func signIn2(phone: String,
-                 password: String,
-                 completion: @escaping (Result<SignInResponse, NetworkError>) -> Void) {
+    func signIn(phone: String,
+                password: String,
+                completion: @escaping (Result<SignInResponse, NetworkError>) -> Void) {
         
         var signIn = URLComponents()
         signIn.scheme = "http"
@@ -58,68 +58,5 @@ final class SignInService {
                 completion(.failure(.postRequestError))
             }
         }
-    }
-    
-    func signIn(phone: String,
-                password: String,
-                completion: @escaping (Result<SignInResponse, NetworkError>) -> Void) {
-        guard let urlLogin = URL(string: apiUrlUserLogin) else {
-            completion(.failure(.badURL))
-            return
-        }
-
-        let signInRequest = SignInRequest(username: phone, password: password)
-        var requestLogin = URLRequest(url: urlLogin)
-        requestLogin.httpMethod = "POST"
-        requestLogin.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        requestLogin.addValue("application/json", forHTTPHeaderField: "Accept")
-
-        let parameters: [String: String] = [
-            "grant_type": "",
-            "username": phone,
-            "password": password,
-            "scope": "",
-            "client_id": "",
-            "client_secret": ""
-        ]
-
-        do {
-            requestLogin.httpBody = try
-            JSONSerialization.data(withJSONObject: parameters, options: .fragmentsAllowed)
-        } catch {
-            completion(.failure(.jsonSerializationError))
-            return
-        }
-//
-//        do {
-//            try JSONEncoder().encode(signInRequest)
-//        } catch {
-//            completion(.failure(.jsonSerializationError))
-//            return
-//        }
-
-        let taskLogin = URLSession.shared.dataTask(with: requestLogin) { data, _, error in
-            if let error = error {
-                print(error)
-                completion(.failure(.postRequestError))
-                return
-            }
-            guard let responseData = data else {
-                completion(.failure(.nilData))
-                return
-            }
-            do {
-                let decodedData = try
-                    JSONDecoder().decode(SignInResponse.self, from: responseData)
-                DispatchQueue.main.async {
-                    completion(.success(decodedData))
-                }
-            } catch let error {
-                print(error)
-                completion(.failure(.decodingError))
-                return
-            }
-        }
-        taskLogin.resume()
     }
 }
