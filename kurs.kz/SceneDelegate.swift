2685a9191f6c8171fbc8c39aaa6ea39832221ba2
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-01 15:32:36 +0600

    [85ztp14e2] Запушила pbxproject

--- kurs.kz/SceneDelegate.swift
+++ kurs.kz/SceneDelegate.swift
@@ -22,7 +22,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let isAutorized = true
         if isAutorized {
             window?.rootViewController =
-                UINavigationController(rootViewController: ToSupportViewController())
+                UINavigationController(rootViewController: FaqViewController())
         } else {
             window?.rootViewController =
                 UINavigationController(rootViewController: SelectLanguageViewController())
