6dcff5fe87041fcfdc954e48cfe38fa24aadc1a4
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-02 12:08:49 +0600

    [85ztp14e2] Создала NationalBankCourseViewController

---
+++ kurs.kz/NationalBankCourse/NationalBankCourseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NationalBankCourseViewController.swift
+//  kurs.kz
+//
+//  Created by Aigerim Abitayeva on 02.08.2023.
+//
+
+import UIKit
+import WebKit
+
+final class NationalBankCourseViewController: UIViewController, WKNavigationDelegate {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
