19d6c494a43a365286a82b3e3fb81572b065fb53
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-03 20:58:52 +0600

    [KK-61] Обновила section data

---
+++ kurs.kz/NewGroup/Model/SectionData.swift
@@ -0,0 +1,23 @@
+//
+//  Section.swift
+//  kurs.kz
+//
+//  Created by Diar Orynbek on 03.08.2023.
+//
+
+import Foundation
+import CollapsibleTableSectionViewController
+
+public struct Section {
+    var name: String
+    var iconImage: UIImage?
+    var items: [String]
+    var collapsed: Bool
+    
+    public init(name: String,iconImage: UIImage?, items: [String], collapsed: Bool = true) {
+        self.name = name
+        self.iconImage = iconImage
+        self.items = items
+        self.collapsed = collapsed
+    }
+}
