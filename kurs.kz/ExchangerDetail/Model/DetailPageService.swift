c41ebc8d52122cb807d3b19c6b631552896f8655
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-04 01:21:19 +0600

    [KK-61] Добавила fetchCurrencyInfo

--- kurs.kz/ExchangerDetail/Model/DetailPageService.swift
+++ kurs.kz/ExchangerDetail/Model/DetailPageService.swift
@@ -12,7 +12,32 @@ final class DetailPageService {
     
     // MARK: - Network
     
-    func fetchDetails(officeID: Int,completion: @escaping (Result<Details, AFError>) -> Void) {
+    func fetchCurrencyInformation(officeID: Int,
+                                  completion: @escaping (Result<[CurrencyElement], AFError>) -> Void) {
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "http"
+        urlComponent.host = "77.240.38.143"
+        urlComponent.port = 4443
+        urlComponent.path = "/exchange_offices/info_rates"
+        urlComponent.query = "office_id=\(officeID)"
+        
+        guard let url = urlComponent.url else {
+            fatalError()
+        }
+        
+        AF.request(url, method: .post, parameters: [:], encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodable(of: [CurrencyElement].self) { response in
+                switch response.result {
+                case .success(let currencyInfo):
+                    completion(.success(currencyInfo))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func fetchDetails(officeID: Int, completion: @escaping (Result<Details, AFError>) -> Void) {
         var urlComponent = URLComponents()
         urlComponent.scheme = "http"
         urlComponent.host = "77.240.38.143"
