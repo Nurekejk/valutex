ed2d89d6adee35f8d300885a976e1929faefc240
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-03 20:59:37 +0600

    [KK-61] Добавила сервис

---
+++ kurs.kz/NewGroup/Model/DetailPageService.swift
@@ -0,0 +1,38 @@
+//
+//  DetailPageService.swift
+//  kurs.kz
+//
+//  Created by Aigerim Abitayeva on 03.08.2023.
+//
+
+import Foundation
+import Alamofire
+
+final class DetailPageService {
+    
+    // MARK: - Network
+    
+    func fetchDetails(officeID: Int,completion: @escaping (Result<Details, AFError>) -> Void) {
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "http"
+        urlComponent.host = "77.240.38.143"
+        urlComponent.port = 4443
+        urlComponent.path = "/exchange_offices/info_main"
+        urlComponent.query = "office_id=\(officeID)"
+        
+        guard let url = urlComponent.url else {
+            fatalError()
+        }
+        
+        AF.request(url, method: .post, parameters: [:], encoding: JSONEncoding.default)
+            .validate()
+            .responseDecodable(of: Details.self) { response in
+                switch response.result {
+                case .success(let details):
+                    completion(.success(details))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+}
