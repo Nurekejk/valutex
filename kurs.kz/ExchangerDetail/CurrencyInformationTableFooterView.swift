19155ae17e71b6e64377b290ececdc25c26f4635
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-04 01:19:07 +0600

    [KK-61] Обновила CurrencyInfo footer

---
+++ kurs.kz/ExchangerDetail/CurrencyInformationTableFooterView.swift
@@ -0,0 +1,50 @@
+//
+//  ExchangeDollarTableViewFooterView.swift
+//  kurs.kz
+//
+//  Created by Diar Orynbek on 14.07.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class CurrencyInformationTableFooterView: UITableViewHeaderFooterView {
+    
+    static let reuseID = String(describing: CurrencyInformationTableFooterView.self)
+    
+    // MARK: - UI
+    private lazy var button: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Еще", for: .normal)
+        button.setTitleColor(AppColor.primaryBase.uiColor, for: .normal)
+        button.titleLabel?.font = AppFont.medium.s14()
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Views
+    private func setupViews() {
+        contentView.backgroundColor = .white
+        contentView.addSubview(button)
+    }
+    
+    // MARK: - Setup Constraints
+    private func setupConstraints() {
+        button.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(8)
+            make.trailing.equalToSuperview().offset(-16)
+            make.bottom.equalToSuperview().offset(-8)
+        }
+    }
+}
