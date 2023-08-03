07b8c0b443dea65fe0b584e2c3ce83c160abaa66
abitoffka <aigerim.abitayevaa@gmail.com>
2023-08-03 20:56:07 +0600

    [KK-61] Удалила sectionsData

--- kurs.kz/NewGroup/DetailViewCollabsibleViewController.swift
+++ kurs.kz/NewGroup/DetailViewCollabsibleViewController.swift
@@ -10,29 +10,8 @@ import SnapKit
 import CollapsibleTableSectionViewController
 
 final class DetailViewCollabsibleViewController: UIViewController {
- 
-    public var sectionsData: [Section] = [
-        Section(name: "Телефон", iconImage: UIImage(named: "phone"),
-                items: [
-                    Item(name: "+7-701-476-99-99"),
-                    Item(name: "+7-701-476-99-99 ")
-                ]),
-        Section(name: "Время работы", iconImage: UIImage(named: "clock"),
-                items: [
-                    Item(name: "Пн - 09:00 - 21:00"),
-                    Item(name: "Вт - 09:00 - 21:00"),
-                    Item(name: "Ср - 09:00 - 21:00"),
-                    Item(name: "Чт - 09:00 - 21:00"),
-                    Item(name: "Пт - 09:00 - 21:00"),
-                    Item(name: "Сб -  выходной"),
-                    Item(name: "Вс -  выходной")
-                ]),
-        Section(name: "Email", iconImage: UIImage(named: "whatsapp"),
-                items: [
-                    Item(name: "mail@mail.ru")
-                ])
-    ]
-
+    
+    private let service: DetailPageService
     var sections = [Section]()
     
     // MARK: - UI
