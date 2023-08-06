9dc2616c4b720a9311a17d8c507c77c0d28a48c4
Akerke <nassyr.akyerke@gmail.com>
2023-08-06 10:05:04 +0600

    [861n49taa] Поменяла скобки

--- kurs.kz/ExchangeRate/ExchangeRateViewController.swift
+++ kurs.kz/ExchangeRate/ExchangeRateViewController.swift
@@ -58,7 +58,7 @@ final class ExchangeRateViewController: UIViewController {
     private func setupViews() {
         
         view.backgroundColor = .systemGray6
-        [headerView, tableView, shadowView].forEach(view.addSubview($0))
+        [headerView, tableView, shadowView].forEach{view.addSubview($0)}
         shadowView.addSubview(safeButton)
     }
     
