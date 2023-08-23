73eee17496a4a851774f5582ae70bdc57cde886b
Akerke <nassyr.akyerke@gmail.com>
2023-08-23 16:30:59 +0600

    [85ztp151x] Реализовала переходы по ссылкам на мессенджеры

--- kurs.kz/Support/ToSupportViewController.swift
+++ kurs.kz/Support/ToSupportViewController.swift
@@ -69,7 +69,7 @@ final class ToSupportViewController: UIViewController {
     }
 }
 
-    // MARK: - UITableViewDataSource, UITableViewDelegate
+// MARK: - UITableViewDataSource, UITableViewDelegate
 extension ToSupportViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -78,10 +78,55 @@ extension ToSupportViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView( _ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SupportTableViewCell.identifier,
-            for: indexPath) as? SupportTableViewCell else {
+                                                       for: indexPath) as? SupportTableViewCell else {
             fatalError("Failed to dequeue the right king of cell.")
         }
         cell.configure(with: supportOptions[indexPath.row])
         return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let row = indexPath.row
+
+        if indexPath.section == SectionNumber.zero.rawValue {
+            switch row {
+
+            case 0:
+                openTelegram()
+
+            case 1:
+                openWhatsApp()
+
+            default:
+                break
+            }
+        }
+    }
+
+    private func openTelegram() {
+        let telegramURL = URL(string: "https://t.me/+77052511606")
+        if UIApplication.shared.canOpenURL(telegramURL!) {
+            UIApplication.shared.open(telegramURL!, options: [:], completionHandler: nil)
+        } else {
+            let appStoreURL = URL(string: "https://apps.apple.com/app/telegram-messenger/id686449807")!
+            UIApplication.shared.open(appStoreURL, options: [:], completionHandler: nil)
+        }
+    }
+    // swiftlint: disable all
+    private func openWhatsApp() {
+        let urlString = "https://api.whatsapp.com/send/?phone=77052511606&text=%D0%B4%D0%BE%D0%B1%D1%80%D1%8B%D0%B9%20%D0%B4%D0%B5%D0%BD%D1%8C,%20%D0%BC%D0%BD%D0%B5%20%D0%BD%D1%83%D0%B6%D0%BD%D0%B0%20%D1%82%D0%B5%D1%85%D0%BD%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BF%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0,%20%D0%B2%20%D1%87%D0%B0%D1%81%D1%82%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20..."
+
+        if let whatsappURL = URL(string: urlString) {
+            if UIApplication.shared.canOpenURL(whatsappURL) {
+                UIApplication.shared.open(whatsappURL, options: [:], completionHandler: nil)
+            } else {
+                if let appStoreURL = URL(string: "https://apps.apple.com/app/whatsapp-messenger/id310633997") {
+                    UIApplication.shared.open(appStoreURL, options: [:], completionHandler: nil)
+                }
+                // swiftlint: enable all
+            }
+        } else {
+            print("Неверный URL для WhatsApp")
+        }
     }
 }
