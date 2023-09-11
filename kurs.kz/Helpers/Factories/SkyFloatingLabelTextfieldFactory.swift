75d6a857161da5d143711d26a5a79107f527c035
Adil Bazylov <adil.bazylov@gmail.com>
2023-09-11 15:42:59 +0600

    [85ztybrmj] добавил методы создания полей

--- kurs.kz/Helpers/Factories/SkyFloatingLabelTextfieldFactory.swift
+++ kurs.kz/Helpers/Factories/SkyFloatingLabelTextfieldFactory.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import SkyFloatingLabelTextField
+
+public protocol TextfieldFactory: AnyObject {
+    func getPasswordTextfield(with placeholder: String) -> PasswordTextField
+    func getRepeatPasswordTextfield() -> PasswordTextField
+    func getTextfield(with placeholder: String) -> SkyFloatingLabelTextField
+}
+
+public class SkyFloatingLabelTextfieldFactory: NSObject, TextfieldFactory {
+    
+    public func getPasswordTextfield(with placeholder: String = "") -> PasswordTextField {
+        let textfield = PasswordTextField(isRepeatPassword: false)
+        if placeholder != "" {
+            textfield.placeholder = placeholder
+        }
+        return textfield
+
+    }
+    
+    public func getRepeatPasswordTextfield() -> PasswordTextField {
+        PasswordTextField(isRepeatPassword: true)
+    }
+    
+    public func getTextfield(with placeholder: String = "") -> SkyFloatingLabelTextField {
+        let textfield = PaddedTextField()
+        if placeholder != "" {
+            textfield.placeholder = placeholder
+        }
+        return textfield
+    }
+}
