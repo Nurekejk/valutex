4dc34a4f02da9cbf302ef31c87b66a181727a725
Adil Bazylov <adil.bazylov@gmail.com>
2023-09-11 15:43:35 +0600

    [85ztybrmj] добавил дефолтные настройки для полей PaddedTextField

--- kurs.kz/Registration/PaddedTextField.swift
+++ kurs.kz/Registration/PaddedTextField.swift
@@ -8,8 +8,30 @@
 import UIKit
 import SkyFloatingLabelTextField
 
-class PaddedTextField: SkyFloatingLabelTextField {
+public class PaddedTextField: SkyFloatingLabelTextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupTextField()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupTextField() {
+        self.placeholderColor = AppColor.gray50.uiColor
+        self.placeholderFont = AppFont.regular.s16()
 
+        self.titleColor = UIColor.lightGray
+        self.titleLabel.font = AppFont.regular.s12()
+        self.selectedTitleColor = AppColor.gray50.uiColor
+        self.textColor = AppColor.gray100.uiColor
+        self.titleFormatter = { $0 }
+        self.keyboardType = .default
+        self.lineView.isHidden = true
+    }
+    
     override open func textRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5))
     }
