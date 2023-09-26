790dac836978868b514a143f293341a7d12e7371
Adil Bazylov <adil.bazylov@gmail.com>
2023-09-26 18:38:37 +0600

    [85zu13cg7] переименовал название модели ячейки -> ReviewForTableView

--- kurs.kz/Exchange Card/Model/ReviewForTableView.swift
+++ kurs.kz/Exchange Card/Model/ReviewForTableView.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+struct ReviewForTableView: Decodable {
+    let fullName: String?
+    let score: Int?
+    let comment: String?
+    let createdAt: String?
+    enum CodingKeys: String, CodingKey {
+        case fullName = "full_name"
+        case score, comment
+        case createdAt = "created_at"
+    }
+}
