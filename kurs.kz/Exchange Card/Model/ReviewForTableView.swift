bb503308b91f21ea07802fcbc6e991160d7f376c
Adil Bazylov <adil.bazylov@gmail.com>
2023-09-26 18:37:05 +0600

    [85zu13cg7] создал файлы для модельки ячейки комментов и файл самой ячейки

---
+++ kurs.kz/Exchange Card/Model/ReviewForTableView.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewForTableView.swift
+//  kurs.kz
+//
+//  Created by MacBook on 26.09.2023.
+//
+
+import Foundation
---
+++ kurs.kz/Exchange Card/ReviewTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  FeedbackTableViewCell.swift
+//  kurs.kz
+//
+//  Created by MacBook on 26.09.2023.
+//
+
+import UIKit
+import SnapKit
+
+final class FeedbackTableViewCell: UITableViewCell {
+    
+}
