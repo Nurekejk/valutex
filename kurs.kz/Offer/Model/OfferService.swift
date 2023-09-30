6d4e92190f2dbdc312ae0cb423aa30b8c45d84c9
Adil Bazylov <adil.bazylov@gmail.com>
2023-09-30 14:22:34 +0600

    удалил мертвый код

--- kurs.kz/Offer/Model/OfferService.swift
+++ kurs.kz/Offer/Model/OfferService.swift
@@ -88,40 +88,4 @@ struct OfferService {
             }
         }
     }
-//    func updateOffer(exchangeAmount: Double,
-//                     exchangeRate: Double) {
-//        let url = "http://77.240.38.143:4443/update_offer"
-//        var headers: HTTPHeaders = [
-//            "accept": "application/json",
-//            "Content-Type": "application/json"
-//        ]
-//        let defaults = UserDefaults.standard
-//        if let data = defaults.data(forKey: SignInViewController.defaultsTokensKey) {
-//            do {
-//                let tokens = try JSONDecoder().decode(SignInResponse.self, from: data)
-//                if let accessToken = tokens.access_token {
-//                    headers["Authorization"] = "Bearer \(String(describing: accessToken))"
-//                }
-//            } catch {
-//                print("error while decoding")
-//            }
-//        }
-//
-//        let parameters: [String: Any] = [
-//            "exchange_amount": exchangeAmount,
-//            "exchange_rate": exchangeRate
-//        ]
-//
-//        AF.request(url, method: .put,
-//                   parameters: parameters,
-//                   encoding: JSONEncoding.default,
-//                   headers: headers).responseJSON { response in
-//            switch response.result {
-//            case .success(let value):
-//                print("Успешный запрос: \(value)")
-//            case .failure(let error):
-//                print("Ошибка запроса: \(error)")
-//            }
-//        }
-//    }
 }
