a56791ffbff05e42072aebc0ecb4f6aeb59eb28f
Adil Bazylov <adil.bazylov@gmail.com>
2023-10-06 17:47:09 +0600

    создал вью модел партнера

--- kurs.kz/PartnerOffer/ViewModel/PartnerOfferViewModel.swift
+++ kurs.kz/PartnerOffer/ViewModel/PartnerOfferViewModel.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+final class PartnerOfferViewModel {
+    
+    // MARK: - Properties
+    private weak var view: PartnerOfferView?
+    private let service: PartnerOfferService
+    
+    private var acceptedOffers: [AcceptedSentOfferResponse] = [] {
+        didSet {
+            view?.updateCounters(acceptedCount: acceptedOffers.count, sentCount: sentOffers.count)
+        }
+    }
+    private var sentOffers: [AcceptedSentOfferResponse] = [] {
+        didSet {
+            view?.updateCounters(acceptedCount: acceptedOffers.count, sentCount: sentOffers.count)
+        }
+    }
+
+    // MARK: - Initializer
+    init(view: PartnerOfferView, service: PartnerOfferService) {
+        self.view = view
+        self.service = service
+    }
+    
+
+    
+}
+
+protocol PartnerOfferView: AnyObject {
+    func updateCounters(acceptedCount: Int, sentCount: Int)
+    func updateOffers()
+    
+}
