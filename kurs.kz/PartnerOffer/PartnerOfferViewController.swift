b892da19bf029187dc9777cd47e256c68bec6b7f
Adil Bazylov <adil.bazylov@gmail.com>
2023-10-09 20:08:14 +0600

    починил таблицу в оффере партнера

--- kurs.kz/PartnerOffer/PartnerOfferViewController.swift
+++ kurs.kz/PartnerOffer/PartnerOfferViewController.swift
@@ -15,14 +15,15 @@ final class PartnerOfferViewController: UIViewController {
     
     // MARK: - UI
     private lazy var acceptedSendTableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .insetGrouped)
+        let tableView = UITableView(frame: .zero, style: .plain)
         tableView.register(AcceptedSentTableViewCell.self,
                            forCellReuseIdentifier: AcceptedSentTableViewCell.reuseID)
         tableView.register(ApplicationTableViewCell.self,
                            forCellReuseIdentifier: ApplicationTableViewCell.reuseID)
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.tableHeaderView?.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .clear
+        tableView.separatorStyle = .none
         return tableView
     }()
     
@@ -46,21 +47,20 @@ final class PartnerOfferViewController: UIViewController {
         setupNavigationBar()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        acceptedSendTableView.layer.cornerRadius = 8
-        acceptedSendTableView.layer.masksToBounds = true
-    }
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        acceptedSendTableView.layer.cornerRadius = 8
+//        acceptedSendTableView.layer.masksToBounds = true
+//    }
     
     // MARK: - Setup Navigation Bar
     private func setupNavigationBar() {
-        edgesForExtendedLayout = []
         self.navigationItem.title = "Оффер"
     }
     
     // MARK: - Setup Views
     private func setupViews() {
-        view.backgroundColor = .systemGray6
+        view.backgroundColor = AppColor.gray10.uiColor
         
         [acceptedSendTableView].forEach {
             view.addSubview($0)
@@ -71,7 +71,8 @@ final class PartnerOfferViewController: UIViewController {
     private func setupConstraints() {
         acceptedSendTableView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(8)
-            make.leading.trailing.bottom.equalToSuperview()
+            make.leading.equalToSuperview().offset(16)
+            make.trailing.equalToSuperview().offset(-16)
             make.bottom.equalToSuperview()
         }
     }
@@ -86,11 +87,14 @@ extension PartnerOfferViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == SectionNumber.zero.rawValue || section == SectionNumber.one.rawValue {
             return 1
+        } else {
+            return viewModel.getNumberOfRequests()
         }
-        return viewModel.getNumberOfRequests()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("trying to configure22")
+
         if (indexPath.section == 0 || indexPath.section == 1) {
             guard let cell =
                     tableView.dequeueReusableCell(withIdentifier: AcceptedSentTableViewCell.reuseID,
@@ -99,7 +103,7 @@ extension PartnerOfferViewController: UITableViewDelegate, UITableViewDataSource
                 fatalError("Could not cast to AcceptedSendTableViewCell")
             }
             viewModel.configureAcceptedSentCell(with: cell, indexPath: indexPath)
-
+            cell.backgroundColor = view.backgroundColor
             return cell
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: ApplicationTableViewCell.reuseID,
@@ -107,6 +111,8 @@ extension PartnerOfferViewController: UITableViewDelegate, UITableViewDataSource
                 fatalError("Could not cast to ApplicationTableViewCell")
             }
             viewModel.configureCell(with: cell, at: indexPath.row)
+            cell.backgroundColor = view.backgroundColor
+
             return cell
         }
     }
@@ -114,12 +120,13 @@ extension PartnerOfferViewController: UITableViewDelegate, UITableViewDataSource
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if (indexPath.section == 0 || indexPath.section == 1) {
             return 80.0
-        } 
-        return 213.0
+        } else {
+            return 230
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return section == 2 ? 60.0 : 8.0
+        return section == 2 ? 50 : 0.0
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
@@ -127,16 +134,9 @@ extension PartnerOfferViewController: UITableViewDelegate, UITableViewDataSource
             return UIView()
         } else {
             let headerView = ApplicationsHeaderView()
+            headerView.setupHeaderCounter(with: viewModel.getNumberOfRequests())
             return headerView
         }
-    }
-    
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 8.0
-    }
-    
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        return UIView()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -154,17 +154,4 @@ extension PartnerOfferViewController: PartnerOfferView {
     func reloadTable() {
         acceptedSendTableView.reloadData()
     }
-    
-    func updateAcceptedCount(acceptedCount: Int) {
-        
-    }
-    
-    func updateSentCount(sentCount: Int) {
-        
-    }
-    
-    func updateRequestsCount(requestsCount: Int) {
-        
-    }
-
 }
