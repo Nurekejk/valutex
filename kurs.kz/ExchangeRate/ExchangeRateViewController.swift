416cda6bdc31a460ca648e8e6356cc0f8f5b241e
Adil Bazylov <adil.bazylov@gmail.com>
2023-10-13 17:48:10 +0600

    добавил динамичную таблицу, сервис фетча курсов, починил хедер и футер

--- kurs.kz/ExchangeRate/ExchangeRateViewController.swift
+++ kurs.kz/ExchangeRate/ExchangeRateViewController.swift
@@ -10,29 +10,46 @@ import SnapKit
 
 final class ExchangeRateViewController: UIViewController {
     
-    // MARK: - UI
-    private lazy var headerView: ExchangeRateTableViewHeaderView = {
-        let headerView = ExchangeRateTableViewHeaderView()
-        return headerView
-    }()
+    // MARK: - Properties
+    private let service: ExchangeRateService
     
+    private var currencyList: [CurrencyElementForExchangeRateVC] = [] {
+        didSet {
+            tableHeight = currencyList.count * 70 + 68
+        }
+    }
+    
+    private var tableHeight = 0 {
+        didSet {
+            tableView.snp.updateConstraints { make in
+                make.height.equalTo(tableHeight)
+            }
+            tableView.reloadData()
+        }
+    }
+    
+    // MARK: - UI
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.dataSource  = self
         tableView.delegate = self
+        tableView.register(ExchangeRateTableViewHeaderView.self,
+                           forHeaderFooterViewReuseIdentifier: ExchangeRateTableViewHeaderView.reuseID)
+        tableView.register(ExchangeRateTableViewFooterView.self,
+                           forHeaderFooterViewReuseIdentifier: ExchangeRateTableViewFooterView.reuseID)
         tableView.register(ExchangeRateTableViewCell.self,
                            forCellReuseIdentifier: ExchangeRateTableViewCell.reuseIdentifier)
-        tableView.tableHeaderView = headerView
-        tableView.tableFooterView = footerView
         tableView.rowHeight = 56
         tableView.separatorStyle = .none
         return tableView
     }()
+    
     private lazy var footerView: ExchangeRateTableViewFooterView = {
         let footerView = ExchangeRateTableViewFooterView()
         return footerView
     }()
-    private lazy var safeButton: UIButton = {
+    
+    private lazy var saveButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Cохранить", for: .normal)
         button.tintColor = AppColor.grayWhite.uiColor
@@ -41,7 +58,8 @@ final class ExchangeRateViewController: UIViewController {
         button.titleLabel?.font = AppFont.semibold.s16()
         return button
     }()
-    private lazy var shadowView: UIView = {
+    
+    private lazy var saveButtonContainerView: UIView = {
         let shadowView = UIView()
         shadowView.backgroundColor = AppColor.grayWhite.uiColor
         return shadowView
@@ -50,8 +68,26 @@ final class ExchangeRateViewController: UIViewController {
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        getCurrencies()
         setupViews()
         setupConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.layer.cornerRadius = 8
+        saveButton.layer.cornerRadius = 12
+    }
+    
+    // MARK: - Initializers
+    init(service: ExchangeRateService) {
+        self.service = service
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     // MARK: - Setup Views
@@ -59,10 +95,10 @@ final class ExchangeRateViewController: UIViewController {
         
         view.backgroundColor = .systemGray6
 
-        [headerView, tableView, shadowView].forEach {
+        [tableView, saveButtonContainerView].forEach {
             view.addSubview($0)
         }
-        shadowView.addSubview(safeButton)
+        saveButtonContainerView.addSubview(saveButton)
     }
     
     // MARK: - Navigation Bar
@@ -70,60 +106,76 @@ final class ExchangeRateViewController: UIViewController {
         title = "Курс валют"
     }
     
-    // MARK: - ViewDid LayoutSubviews
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        tableView.layer.cornerRadius = 8
-        safeButton.layer.cornerRadius = 12
+    // MARK: - Action
+    private func getCurrencies() {
+        service.fetchCurrencyRates { [weak self] result in
+            switch result {
+            case .success(let result):
+                self?.currencyList = result
+                print("currencies are: \(result)")
+            case .failure:
+                print("error fetching currencies")
+            }
+        }
     }
-        
+    
     // MARK: - Setup Constraints
     private func setupConstraints() {
-        safeButton.snp.makeConstraints { make in
+        
+        saveButton.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(16)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
             make.height.equalTo(52)
         }
-        shadowView.snp.makeConstraints { make in
+        saveButtonContainerView.snp.makeConstraints { make in
             make.bottom.equalToSuperview()
             make.leading.equalToSuperview()
             make.trailing.equalToSuperview()
             make.height.equalTo(112)
         }
-        headerView.frame = CGRect(x: 0, y: 0, width: 0, height: 36)
-        footerView.frame = CGRect(x: 0, y: 0, width: 0, height: 46)
 
         tableView.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(120)
+            make.top.equalToSuperview().offset(13)
             make.leading.equalToSuperview().offset(16)
             make.trailing.equalToSuperview().offset(-16)
-            make.height.equalTo(260)
+            make.height.equalTo(tableHeight)
         }
     }
 }
 
 // MARK: - UITableViewDataSource, UITableViewDelegate
 extension ExchangeRateViewController: UITableViewDataSource, UITableViewDelegate {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: ExchangeRateTableViewHeaderView.reuseID) as? ExchangeRateTableViewHeaderView
+        else { return UITableViewHeaderFooterView() }
+        return header
     }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        guard let footer = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: ExchangeRateTableViewFooterView.reuseID) as? ExchangeRateTableViewFooterView
+        else { return UITableViewHeaderFooterView() }
+        return footer
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currencyList.count
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: ExchangeRateTableViewCell.reuseIdentifier,
             for: indexPath) as? ExchangeRateTableViewCell else {
             fatalError("Could not cast to ExchangeRateTableViewCell")
         }
-        switch indexPath.row {
-        case 0:
-            cell.configureCell(flagImage: AppImage.KZTflag.uiImage,
-                                currencyLabel: "Доллар",
-                                amountOfPurchaseTextField: "500",
-                                amountOfSaleTextField: "500",
-                                trashButton: AppImage.trash.uiImage)
-        default:
-            break
-        }
+        let currency = currencyList[indexPath.row]
+        cell.configureCell(flagImage: currency.currencyFlag ?? "",
+                           currencyLabel: currency.currencyRuName ?? "",
+                           buyRate: currency.buyPrice,
+                           sellRate: currency.sellPrice)
         return cell
     }
 }
