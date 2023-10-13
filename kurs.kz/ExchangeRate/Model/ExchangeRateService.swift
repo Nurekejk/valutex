2d2b533291a5079d851e57198fd0899e1c1dd609
Adil Bazylov <adil.bazylov@gmail.com>
2023-10-13 17:48:58 +0600

    создал сервис

--- kurs.kz/ExchangeRate/Model/ExchangeRateService.swift
+++ kurs.kz/ExchangeRate/Model/ExchangeRateService.swift
@@ -6,3 +6,90 @@
 //
 
 import Foundation
+import Alamofire
+
+struct ExchangeRateService {
+    
+    // MARK: - Network
+    func fetchCurrencyRates(completion: @escaping (Result<[CurrencyElementForExchangeRateVC],
+                                                    AFError>) -> Void) {
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "https"
+        urlComponent.host = "api.valutex.kz"
+        urlComponent.path = "/profile/currency_rates"
+
+        guard let url = urlComponent.url else {
+            return
+        }
+        
+        var headers: HTTPHeaders = [
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        ]
+        
+        getAuth(&headers)
+
+        AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers)
+        .validate()
+        .responseDecodable(of: [CurrencyElementForExchangeRateVC].self) { response in
+            switch response.result {
+            case .success(let message):
+                completion(.success(message))
+            case .failure(let error):
+                print(error)
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func fetchOffers(controllerType: PartnerOfferType,
+                            completion: @escaping (Result<[AcceptedSentOfferResponse], AFError>) -> Void) {
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "https"
+        urlComponent.host = "api.valutex.kz"
+        switch controllerType {
+        case .accepted:
+            urlComponent.path = "/accepted_offers_response_list"
+        case .sent:
+            urlComponent.path = "/sent_offers_response_list"
+        }
+
+        guard let url = urlComponent.url else {
+            return
+        }
+        
+        var headers: HTTPHeaders = [
+            "accept": "application/json",
+            "Content-Type": "application/json"
+        ]
+        
+        getAuth(&headers)
+
+        AF.request(url, method: .get, encoding: JSONEncoding.default, headers: headers)
+        .validate()
+        .responseDecodable(of: [AcceptedSentOfferResponse].self) { response in
+            switch response.result {
+            case .success(let message):
+                completion(.success(message))
+            case .failure(let error):
+                print(error)
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    private func getAuth(_ headers: inout HTTPHeaders) {
+        let defaults = UserDefaults.standard
+        if let data = defaults.data(forKey: SignInViewController.defaultsUserAndTokensKey) {
+            do {
+                let tokens = try JSONDecoder().decode(SignInResponse.self, from: data)
+                if let accessToken = tokens.access_token {
+                    
+                    headers["Authorization"] = "Bearer \(accessToken)"
+                }
+            } catch {
+                print("error while decoding")
+            }
+        }
+    }
+}
