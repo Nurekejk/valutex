d510a6351b1a78533be8bd87392de19a54e4357c
Adil Bazylov <adil.bazylov@gmail.com>
2023-10-13 17:49:10 +0600

    создал структуру CurrencyElementForExchangeRateVC

--- kurs.kz/ExchangeRate/Model/CurrencyElementForExchangeRateVC.swift
+++ kurs.kz/ExchangeRate/Model/CurrencyElementForExchangeRateVC.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct CurrencyElementForExchangeRateVC: Decodable {
+    let currencyRateId: Int?
+    let currencyCode, currencyFlag, currencyKzName, currencyRuName, currencyEngName: String?
+    let buyPrice, sellPrice: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case currencyRateId = "currency_rate_id"
+        case currencyCode = "currency_code"
+        case currencyFlag = "currency_flag"
+        case currencyKzName = "currency_kz_name"
+        case currencyRuName = "currency_ru_name"
+        case currencyEngName = "currency_eng_name"
+        case buyPrice = "buy_price"
+        case sellPrice = "sell_price"
+    }
+}
